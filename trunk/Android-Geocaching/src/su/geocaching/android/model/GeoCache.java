71c6309d0d1b4ecf1665702d198ae789725cdcba
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-03 16:55:57 +0000

    added the String field "name" in class GeoCache

--- trunk/Android-Geocaching/src/su/geocaching/android/model/GeoCache.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/GeoCache.java
@@ -9,26 +9,53 @@ import com.google.android.maps.GeoPoint;
  */
 public class GeoCache {
     private GeoPoint locationGeoPoint;	//Coordinates of cache location
-    private int id;		//Unique identifier of GeoCache(from geocaching.su)
+    private int id;			//Unique identifier of GeoCache(from geocaching.su)
+    private String name;
 
+    
+    public GeoCache() {	
+	locationGeoPoint = new GeoPoint(0, 0);
+    }  
+    
     // TODO: if cache id not found - throw exception?
     public GeoCache(int id) {
 	this.id = id;
 
 	// TODO: retrieve data from API Manager
 	locationGeoPoint = new GeoPoint(59879936, 29828610);
-    }
+    }     
 
     public GeoCache(int latitude, int longitude, int id) {
 	this.id = id;	
 	locationGeoPoint = new GeoPoint(latitude, longitude);
     }
     
-    public GeoPoint getLocation() {
+    public GeoCache(int latitude, int longitude, int id, String name) {
+	this.id = id;	
+	locationGeoPoint = new GeoPoint(latitude, longitude);
+    }    
+    
+    public GeoPoint getLocationGeoPoint() {
 	return locationGeoPoint;
     }
 
     public int getId() {
 	return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLocationGeoPoint(GeoPoint locationGeoPoint) {
+        this.locationGeoPoint = locationGeoPoint;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
