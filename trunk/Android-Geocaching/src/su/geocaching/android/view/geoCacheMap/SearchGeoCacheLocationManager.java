389d96b33ffe459ab9167f72aa7bff3f839bfb2c
grigory.kalabin <grigory.kalabin@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-06 16:25:18 +0000

    icons from drawable-hdpi,ldpi,mdpi moved to drawable. Deleted UserLocationOverlay. Instead this using MyLocationOverlay. It works fine. Add 2 i-faces for Activities with compass and location service. Small refactoring in SearchGeoCache*

--- trunk/Android-Geocaching/src/su/geocaching/android/view/geoCacheMap/SearchGeoCacheLocationManager.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/view/geoCacheMap/SearchGeoCacheLocationManager.java
@@ -1,6 +1,5 @@
 package su.geocaching.android.view.geoCacheMap;
 
-import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -9,20 +8,21 @@ import android.os.Bundle;
 
 public class SearchGeoCacheLocationManager implements LocationListener {
 
-    private Context context;
-    private LocationManager lmLocationManager;
+    private IActivityWithLocation context;
+    private LocationManager locationManager;
     private Location lastLocation;
+    private String provider;
 
-    public SearchGeoCacheLocationManager(Context context) {
-        this.context = context;
-        lmLocationManager = (LocationManager) context
-                .getSystemService(Context.LOCATION_SERVICE);
-        lastLocation = lmLocationManager
-                .getLastKnownLocation(Context.LOCATION_SERVICE);
-        if (lastLocation != null) {
-            updateLocation();
-        }
-        resume();
+    public SearchGeoCacheLocationManager(IActivityWithLocation context,
+	    LocationManager locationManager) {
+	this.context = context;
+	this.locationManager = locationManager;
+	resume();
+	lastLocation = this.locationManager
+		.getLastKnownLocation(provider);
+	if (lastLocation != null) {
+	    updateLocation();
+	}
     }
 
     /**
@@ -30,50 +30,47 @@ public class SearchGeoCacheLocationManager implements LocationListener {
      */
     @Override
     public void onLocationChanged(Location location) {
-        lastLocation = location;
-        updateLocation();
+	lastLocation = location;
+	updateLocation();
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        // TODO: implement onProviderDisabled
+	// TODO: implement onProviderDisabled
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        // TODO: implement onProviderEnabled
+	// TODO: implement onProviderEnabled
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO: implement onStatusChanged
+	// TODO: implement onStatusChanged
     }
 
     /**
      * Remove updates, when need to pause work
      */
     public void pause() {
-        lmLocationManager.removeUpdates(this);
+	locationManager.removeUpdates(this);
     }
 
     /**
      * Add updates after pause.
      */
     public void resume() {
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        String provider = lmLocationManager.getBestProvider(criteria, true);
-        lmLocationManager.requestLocationUpdates(provider, 1000, 1, this);
+	Criteria criteria = new Criteria();
+	criteria.setAccuracy(Criteria.ACCURACY_FINE);
+	provider = locationManager.getBestProvider(criteria, true);
+	locationManager.requestLocationUpdates(provider, 1000, 1, this);
     }
 
     public Location getCurrentLocation() {
-        return lastLocation;
+	return lastLocation;
     }
 
     private void updateLocation() {
-        if (context instanceof GeoCacheMap) {
-            float azimuth = ((GeoCacheMap) context).getLastAzimuth();
-            ((GeoCacheMap) context).updateUserOverlay(lastLocation, azimuth);
-        }
+	context.updateLocation(lastLocation);
     }
 }
--- trunk/Android-Geocaching/src/su/geocaching/android/view/userStory/searchGeoCache/SearchGeoCacheMap.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/view/userStory/searchGeoCache/SearchGeoCacheMap.java
@@ -1,15 +1,19 @@
 package su.geocaching.android.view.userStory.searchGeoCache;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.os.Bundle;
-import com.google.android.maps.OverlayItem;
+import su.geocaching.android.controller.Controller;
 import su.geocaching.android.model.dataType.GeoCache;
 import su.geocaching.android.view.MainMenu;
 import su.geocaching.android.view.R;
 import su.geocaching.android.view.geoCacheMap.GeoCacheItemizedOverlay;
 import su.geocaching.android.view.geoCacheMap.GeoCacheMap;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.OverlayItem;
 
 /**
  * @author Android-Geocaching.su student project team
@@ -22,30 +26,53 @@ public class SearchGeoCacheMap extends GeoCacheMap {
     private GeoCache geoCache;
     private OverlayItem cacheOverlayItem;
     private GeoCacheItemizedOverlay cacheItemizedOverlay;
-
+    
+    private MyLocationOverlay userOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.search_geocache_map);
         Intent intent = this.getIntent();
+        Controller controller = Controller.getInstance();
+        
+        //not working yet
+        //geoCache = controller.getGeoCacheByID(intent.getIntExtra(
+        //        MainMenu.DEFAULT_GEOCACHE_ID_NAME, -1));
         geoCache = new GeoCache(intent.getIntExtra(
                 MainMenu.DEFAULT_GEOCACHE_ID_NAME, -1));
+        
+        map = (MapView) findViewById(R.id.searchGeocacheMap);
+        userOverlay = new MyLocationOverlay(this, map);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        
         Drawable cacheMarker = this.getResources().getDrawable(R.drawable.orangecache);
         cacheMarker.setBounds(0,
                 -cacheMarker.getMinimumHeight(),
                 cacheMarker.getMinimumWidth(), 0);
+        
+        userOverlay.enableCompass();
+        userOverlay.enableMyLocation();
+        
         cacheItemizedOverlay = new GeoCacheItemizedOverlay(cacheMarker);
         cacheOverlayItem = new OverlayItem(geoCache.getLocationGeoPoint(), "", "");
         cacheItemizedOverlay.addOverlay(cacheOverlayItem);
         mapOverlays.add(cacheItemizedOverlay);
-        mvMap.invalidate();
-        mcMapController.animateTo(geoCache.getLocationGeoPoint());
+        mapOverlays.add(userOverlay);
+        map.invalidate();
+        mapController.animateTo(geoCache.getLocationGeoPoint());
+    }
+    
+    @Override
+    protected void onPause() {
+	super.onPause();
+	
+	userOverlay.disableCompass();
+	userOverlay.disableMyLocation();
     }
 
     private void startCompassView() {
@@ -56,7 +83,27 @@ public class SearchGeoCacheMap extends GeoCacheMap {
     }
 
     protected float getDistanceToGeoCache(Location location) {
-        //TODO: implement using Location.distanceBetween
-        return 0;
+	float[] results = new float[3];
+	double endLatitude = geoCache.getLocationGeoPoint().getLatitudeE6() / 1E6;
+	double endLongitude = geoCache.getLocationGeoPoint().getLongitudeE6() / 1E6;
+	Location.distanceBetween(location.getLatitude(),
+		location.getLongitude(), 
+		endLatitude, endLongitude, results);
+        return results[0];
+    }
+
+    @Override
+    public void updateLocation(Location location) {
+	// TODO show distance between
+	
+    }
+
+    public float getLastAzimuth() {
+	return userOverlay.getOrientation();
+    }
+
+    @Override
+    public Location getLastLocation() {
+	return userOverlay.getLastFix();
     }
 }
