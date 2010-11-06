eef227289ea789fc13438f1ad2bbb9cf04a35903
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-06 14:12:12 +0000

    Add enum to GeoCache class

--- trunk/Android-Geocaching/src/su/geocaching/android/model/dataType/GeoCache.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/dataType/GeoCache.java
@@ -6,93 +6,74 @@ import java.util.HashMap;
 
 /**
  * @author Nikita Bumakov
- * @since October 2010
- *        GeoCache - central concept of geocaching game.
+ * @since October 2010 GeoCache - central concept of geocaching game.
  */
 public class GeoCache {
-    private GeoPoint locationGeoPoint;    //Coordinates of cache location
-    private int id;            //Unique identifier of GeoCache(from geocaching.su)
-
-    //TODO: maybe implement a database of parameters
-    private HashMap<String, String> param;
-
-    private void initParam() {
-        param = new HashMap<String, String>();
-
-        param.put(NAME, "name of cache");
-        param.put(TYPE, TYPE_TRADITIONAL);        
-        param.put(STATUS, STATUS_VALID);       
-        //TODO: add more parameters of cache
-    }
-
-    public HashMap<String, String> getParam() {
-        return param;
-    }
+    private GeoPoint locationGeoPoint; // Coordinates of cache location
+    private int id; // Unique identifier of GeoCache(from geocaching.su)
+    private String name;
+    private TypeOfGeocache type;
+    private StatusOfGeoCache status;
 
     public GeoCache() {
-        locationGeoPoint = new GeoPoint(0, 0);
-        initParam();
+	locationGeoPoint = new GeoPoint(0, 0);
     }
 
     public GeoCache(int id) {
-        this.id = id;
-        initParam();
-        // TODO: retrieve data from API Manager
-        locationGeoPoint = new GeoPoint(59879936, 29828610);
+	this.id = id;
+	// TODO: retrieve data from API Manager
+	locationGeoPoint = new GeoPoint(59879936, 29828610);
     }
 
     public GeoCache(int latitude, int longitude, int id) {
-        this.id = id;
-        locationGeoPoint = new GeoPoint(latitude, longitude);
-        initParam();
+	this.id = id;
+	locationGeoPoint = new GeoPoint(latitude, longitude);
     }
 
     public GeoCache(int latitude, int longitude, int id, String name) {
-        this.id = id;
-        locationGeoPoint = new GeoPoint(latitude, longitude);
+	this.id = id;
+	locationGeoPoint = new GeoPoint(latitude, longitude);
+	this.name = name;
     }
 
     public GeoPoint getLocationGeoPoint() {
-        return locationGeoPoint;
+	return locationGeoPoint;
     }
 
     public int getId() {
-        return id;
+	return id;
     }
 
     public void setLocationGeoPoint(GeoPoint locationGeoPoint) {
-        this.locationGeoPoint = locationGeoPoint;
+	this.locationGeoPoint = locationGeoPoint;
     }
 
     public void setId(int id) {
-        this.id = id;
+	this.id = id;
     }
 
-    public String getGeoCachParameter(String parametrId){
-	return param.get(parametrId);
+    public String getName() {
+	return name;
     }
-    
-    public void setGeoCachParameter(String parametrId, String value){
-	param.put(parametrId, value);
+
+    public void setName(String name) {
+	this.name = name;
     }
-    
-    //parameter types
-    public static final String NAME = "name";
-    public static final String TYPE = "type";
-    public static final String STATUS = "status";
-    
-    //types of caches
-    public static final String TYPE_TRADITIONAL = "traditional";
-    public static final String TYPE_VIRTUAL = "virtual";
-    public static final String TYPE_STEP_BY_STEP = "step by step";
-    public static final String TYPE_EXTREME = "extreme";
-    public static final String TYPE_EVENT = "event";
-  
-    
-    //statuses of caches
-    public static final String STATUS_VALID = "valid";
-    public static final String STATUS_NOT_VALID = "not valid";
-    public static final String STATUS_NOT_CONFIRMED = "not confirmed";
-    
-    
+
+    public TypeOfGeocache getType() {
+	return type;
+    }
+
+    public void setType(TypeOfGeocache type) {
+	this.type = type;
+    }
+
+    public StatusOfGeoCache getStatus() {
+	return status;
+    }
+
+    public void setStatus(StatusOfGeoCache status) {
+	this.status = status;
+    }
+
 }
