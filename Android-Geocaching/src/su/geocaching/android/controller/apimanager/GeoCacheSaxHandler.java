7f2a7764ac38bd8119867d7b0248a3906baf99ff
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-07 15:02:32 +0000

    Change GeoCacheSaxHandler according to new version of GeoCache

--- Android-Geocaching/src/su/geocaching/android/controller/apimanager/GeoCacheSaxHandler.java
+++ Android-Geocaching/src/su/geocaching/android/controller/apimanager/GeoCacheSaxHandler.java
@@ -5,6 +5,8 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 import su.geocaching.android.model.datatype.GeoCache;
+import su.geocaching.android.model.datatype.StatusOfGeoCache;
+import su.geocaching.android.model.datatype.TypeOfGeoCache;
 
 import java.util.LinkedList;
 
@@ -29,138 +31,134 @@ import java.util.LinkedList;
  */
 public class GeoCacheSaxHandler extends DefaultHandler {
 
-    private final static String C = "c";
-    private final static String ID = "id";
-    private final static String CN = "cn";
-    private final static String A = "a";
-    private final static String NAME = "n";
-    private final static String LATITUDE = "la";
-    private final static String LONGITUDE = "ln";
-    private final static String CACH_TYPE = "ct";
-    private final static String STATUS = "st";
+	private final static String C = "c";
+	private final static String ID = "id";
+	private final static String CN = "cn";
+	private final static String A = "a";
+	private final static String NAME = "n";
+	private final static String LATITUDE = "la";
+	private final static String LONGITUDE = "ln";
+	private final static String CACH_TYPE = "ct";
+	private final static String STATUS = "st";
 
-    private GeoCache geoCache;
-    private int latitude, longitude;
-    private String text;
-    private LinkedList<GeoCache> geoCacheList;
+	private GeoCache geoCache;
+	private int latitude, longitude;
+	private String text;
+	private LinkedList<GeoCache> geoCacheList;
 
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        text = new String(ch, start, length).trim();
-        super.characters(ch, start, length);
-    }
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		text = new String(ch, start, length).trim();
+		super.characters(ch, start, length);
+	}
 
-    @Override
-    public void endDocument() throws SAXException {
-        // TODO Auto-generated method stub
-        super.endDocument();
-    }
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.endDocument();
+	}
 
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
 
-        if (localName.equalsIgnoreCase(ID)) {
-            geoCache.setId(Integer.parseInt(text));
-        } else if (localName.equalsIgnoreCase(CN)) {
-            // TODO What is CN?
-        } else if (localName.equalsIgnoreCase(A)) {
-            // TODO What is A?
-        } else if (localName.equalsIgnoreCase(NAME)) {
-            geoCache.setName(text);
-        } else if (localName.equalsIgnoreCase(LATITUDE)) {
-            latitude = (int) (Double.parseDouble(text) * 1E6);
-        } else if (localName.equalsIgnoreCase(LONGITUDE)) {
-            longitude = (int) (Double.parseDouble(text) * 1E6);
-            geoCache.setLocationGeoPoint(new GeoPoint(latitude, longitude));
-        } else if (localName.equalsIgnoreCase(CACH_TYPE)) {
-            int type = Integer.parseInt(text);
-            setGeoCacheType(type);
-        } else if (localName.equalsIgnoreCase(STATUS)) {
-            int status = Integer.parseInt(text);
-            setGeoCacheStatus(status);
-        } else if (localName.equalsIgnoreCase(C)) {
-            geoCacheList.add(geoCache);
-        }
+		if (localName.equalsIgnoreCase(ID)) {
+			geoCache.setId(Integer.parseInt(text));
+		} else if (localName.equalsIgnoreCase(CN)) {
+			// TODO What is CN?
+		} else if (localName.equalsIgnoreCase(A)) {
+			// TODO What is A?
+		} else if (localName.equalsIgnoreCase(NAME)) {
+			geoCache.setName(text);
+		} else if (localName.equalsIgnoreCase(LATITUDE)) {
+			latitude = (int) (Double.parseDouble(text) * 1E6);
+		} else if (localName.equalsIgnoreCase(LONGITUDE)) {
+			longitude = (int) (Double.parseDouble(text) * 1E6);
+			geoCache.setLocationGeoPoint(new GeoPoint(latitude, longitude));
+		} else if (localName.equalsIgnoreCase(CACH_TYPE)) {
+			int type = Integer.parseInt(text);
+			setGeoCacheType(type);
+		} else if (localName.equalsIgnoreCase(STATUS)) {
+			int status = Integer.parseInt(text);
+			setGeoCacheStatus(status);
+		} else if (localName.equalsIgnoreCase(C)) {
+			geoCacheList.add(geoCache);
+		}
 
-        if (localName.equalsIgnoreCase(ID)) {
-            geoCache.setId(Integer.parseInt(text));
-        } else if (localName.equalsIgnoreCase(CN)) {
-            // TODO What is CN?
-        } else if (localName.equalsIgnoreCase(A)) {
-            // TODO What is A?
-        } else if (localName.equalsIgnoreCase(NAME)) {
-            geoCache.setName(text);
-        } else if (localName.equalsIgnoreCase(LATITUDE)) {
-            latitude = (int) (Double.parseDouble(text) * 1E6);
-        } else if (localName.equalsIgnoreCase(LONGITUDE)) {
-            longitude = (int) (Double.parseDouble(text) * 1E6);
-            geoCache.setLocationGeoPoint(new GeoPoint(latitude, longitude));
-        } else if (localName.equalsIgnoreCase(CACH_TYPE)) {
-            int type = Integer.parseInt(text);
-            setGeoCacheType(type);
-        } else if (localName.equalsIgnoreCase(STATUS)) {
-            int status = Integer.parseInt(text);
-            setGeoCacheStatus(status);
-        } else if (localName.equalsIgnoreCase(C)) {
-            geoCacheList.add(geoCache);
-        }
+		if (localName.equalsIgnoreCase(ID)) {
+			geoCache.setId(Integer.parseInt(text));
+		} else if (localName.equalsIgnoreCase(CN)) {
+			// TODO What is CN?
+		} else if (localName.equalsIgnoreCase(A)) {
+			// TODO What is A?
+		} else if (localName.equalsIgnoreCase(NAME)) {
+			geoCache.setName(text);
+		} else if (localName.equalsIgnoreCase(LATITUDE)) {
+			latitude = (int) (Double.parseDouble(text) * 1E6);
+		} else if (localName.equalsIgnoreCase(LONGITUDE)) {
+			longitude = (int) (Double.parseDouble(text) * 1E6);
+			geoCache.setLocationGeoPoint(new GeoPoint(latitude, longitude));
+		} else if (localName.equalsIgnoreCase(CACH_TYPE)) {
+			int type = Integer.parseInt(text);
+			setGeoCacheType(type);
+		} else if (localName.equalsIgnoreCase(STATUS)) {
+			int status = Integer.parseInt(text);
+			setGeoCacheStatus(status);
+		} else if (localName.equalsIgnoreCase(C)) {
+			geoCacheList.add(geoCache);
+		}
 
-        super.endElement(uri, localName, qName);
-    }
+		super.endElement(uri, localName, qName);
+	}
 
-    private void setGeoCacheType(int type) {
-        // switch (type) {
-        // case 1:
-        // geoCache.setGeoCachParameter(GeoCache.TYPE,
-        // GeoCache.TYPE_TRADITIONAL);
-        // break;
-        // case 2:
-        // geoCache.setGeoCachParameter(GeoCache.TYPE,
-        // GeoCache.TYPE_STEP_BY_STEP);
-        // break;
-        // case 3:
-        // geoCache.setGeoCachParameter(GeoCache.TYPE, GeoCache.TYPE_VIRTUAL);
-        // break;
-        // case 4:
-        // geoCache.setGeoCachParameter(GeoCache.TYPE, GeoCache.TYPE_EVENT);
-        // break;
-        // case 6:
-        // geoCache.setGeoCachParameter(GeoCache.TYPE, GeoCache.TYPE_EXTREME);
-        // break;
-        // }
+	private void setGeoCacheType(int type) {
+		switch (type) {
+		case 1:
+			geoCache.setType(TypeOfGeoCache.TRADITIONAL);
+			break;
+		case 2:
+			geoCache.setType(TypeOfGeoCache.STEP_BY_STEP);
+			break;
+		case 3:
+			geoCache.setType(TypeOfGeoCache.VIRTUAL);
+			break;
+		case 4:
+			geoCache.setType(TypeOfGeoCache.EVENT);
+			break;
+		case 6:
+			geoCache.setType(TypeOfGeoCache.EXTREME);
+			break;
+		}
 
-    }
+	}
 
-    private void setGeoCacheStatus(int status) {
-        // switch (status) {
-        // case 1:
-        // geoCache.setGeoCachParameter(GeoCache.STATUS, GeoCache.STATUS_VALID);
-        // break;
-        // case 2:
-        // geoCache.setGeoCachParameter(GeoCache.STATUS,
-        // GeoCache.STATUS_NOT_VALID);
-        // break;
-        // case 3:
-        // geoCache.setGeoCachParameter(GeoCache.STATUS,
-        // GeoCache.STATUS_NOT_CONFIRMED);
-        // break;
-        // }
-    }
+	private void setGeoCacheStatus(int status) {
+		switch (status) {
+		case 1:
+			geoCache.setStatus(StatusOfGeoCache.VALID);
+			break;
+		case 2:
+			geoCache.setStatus(StatusOfGeoCache.NOT_VALID);
+			break;
+		case 3:
+			geoCache.setStatus(StatusOfGeoCache.NOT_CONFIRMED);
+			break;
+		}
+	}
 
-    @Override
-    public void startDocument() throws SAXException {
-        geoCacheList = new LinkedList<GeoCache>();
-        super.startDocument();
-    }
+	@Override
+	public void startDocument() throws SAXException {
+		geoCacheList = new LinkedList<GeoCache>();
+		super.startDocument();
+	}
 
-    @Override
-    public void startElement(String uri, String localName, String name, Attributes attributes) throws SAXException {
-        if (localName.equalsIgnoreCase(C)) {
-            geoCache = new GeoCache();
-        }
-    }
+	@Override
+	public void startElement(String uri, String localName, String name, Attributes attributes) throws SAXException {
+		if (localName.equalsIgnoreCase(C)) {
+			geoCache = new GeoCache();
+		}
+	}
 
-    public LinkedList<GeoCache> getGeoCaches() {
-        return geoCacheList;
-    }
+	public LinkedList<GeoCache> getGeoCaches() {
+		return geoCacheList;
+	}
 }
