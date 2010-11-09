5201686e4141cc80ab0e9618b11fffd0af696268
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-09 17:45:46 +0000

    1) remove unnecessary constructors from GeoCache;  2) public GeoCache(int id) marked as deprecated

--- Android-Geocaching/src/su/geocaching/android/model/datatype/GeoCache.java
+++ Android-Geocaching/src/su/geocaching/android/model/datatype/GeoCache.java
@@ -7,71 +7,60 @@ import com.google.android.maps.GeoPoint;
  * @since October 2010 GeoCache - central concept of geocaching game.
  */
 public class GeoCache {
-    private GeoPoint locationGeoPoint; // Coordinates of cache location
     private int id; // Unique identifier of GeoCache(from geocaching.su)
+    private GeoPoint locationGeoPoint;   
     private String name;
     private GeoCacheType type;
     private GeoCacheStatus status;
 
     public GeoCache() {
-        locationGeoPoint = new GeoPoint(0, 0);
+	locationGeoPoint = new GeoPoint(0, 0);
     }
 
+    @Deprecated
     public GeoCache(int id) {
-        this.id = id;
-        // TODO: retrieve data from API Manager
-        locationGeoPoint = new GeoPoint(59879936, 29828610);
-    }
-
-    public GeoCache(int latitude, int longitude, int id) {
-        this.id = id;
-        locationGeoPoint = new GeoPoint(latitude, longitude);
-    }
-
-    public GeoCache(int latitude, int longitude, int id, String name) {
-        this.id = id;
-        locationGeoPoint = new GeoPoint(latitude, longitude);
-        this.name = name;
+	this.id = id;
+	locationGeoPoint = new GeoPoint(59879936, 29828610);
     }
 
     public GeoPoint getLocationGeoPoint() {
-        return locationGeoPoint;
+	return locationGeoPoint;
+    }
+    
+    public void setLocationGeoPoint(GeoPoint locationGeoPoint) {
+	this.locationGeoPoint = locationGeoPoint;
     }
 
     public int getId() {
-        return id;
+	return id;
     }
-
-    public void setLocationGeoPoint(GeoPoint locationGeoPoint) {
-        this.locationGeoPoint = locationGeoPoint;
-    }
-
+    
     public void setId(int id) {
-        this.id = id;
+	this.id = id;
     }
 
     public String getName() {
-        return name;
+	return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
 
     public GeoCacheType getType() {
-        return type;
+	return type;
     }
 
     public void setType(GeoCacheType type) {
-        this.type = type;
+	this.type = type;
     }
 
     public GeoCacheStatus getStatus() {
-        return status;
+	return status;
     }
 
     public void setStatus(GeoCacheStatus status) {
-        this.status = status;
+	this.status = status;
     }
 
 }
