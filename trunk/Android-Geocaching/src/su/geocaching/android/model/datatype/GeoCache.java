ff20ad45e7320b3acb61aba4cc0b7ebb89acd510
grigory.kalabin <grigory.kalabin@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-19 20:29:27 +0000

    Implemented Parselable interface in GeoCache instead Serializable.

--- trunk/Android-Geocaching/src/su/geocaching/android/model/datatype/GeoCache.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/datatype/GeoCache.java
@@ -1,6 +1,7 @@
 package su.geocaching.android.model.datatype;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.google.android.maps.GeoPoint;
 
@@ -8,7 +9,8 @@ import com.google.android.maps.GeoPoint;
  * @author Nikita Bumakov
  * @since October 2010 GeoCache - central concept of geocaching game.
  */
-public class GeoCache implements Serializable {
+public class GeoCache implements Parcelable {
+
     private int id; // Unique identifier of GeoCache(from geocaching.su)
     private GeoPoint locationGeoPoint;
     private String name;
@@ -25,6 +27,8 @@ public class GeoCache implements Serializable {
     public GeoCache(int id) {
 	this.id = id;
 	locationGeoPoint = new GeoPoint(59879936, 29828610);
+	type = GeoCacheType.TRADITIONAL;
+	status = GeoCacheStatus.VALID;
     }
 
     public GeoPoint getLocationGeoPoint() {
@@ -67,4 +71,65 @@ public class GeoCache implements Serializable {
 	this.status = status;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.os.Parcelable#describeContents()
+     */
+    @Override
+    public int describeContents() {
+	return 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.os.Parcelable#writeToParcel(android.os.Parcel, int)
+     */
+    @Override
+    public void writeToParcel(Parcel arg0, int arg1) {
+	// ! Important order of writing to parcel
+	arg0.writeInt(id);
+	arg0.writeString(name);
+	int[] data = new int[2];
+	data[0] = locationGeoPoint.getLatitudeE6();
+	data[1] = locationGeoPoint.getLongitudeE6();
+	arg0.writeIntArray(data);
+	arg0.writeInt(type.ordinal());
+	arg0.writeInt(status.ordinal());
+    }
+
+    /**
+     * Standard implementation of static final class which need for creating
+     * GeoCache object from parcel
+     */
+    public static final Parcelable.Creator<GeoCache> CREATOR = new Parcelable.Creator<GeoCache>() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.os.Parcelable.Creator#createFromParcel(android.os.Parcel)
+	 */
+	public GeoCache createFromParcel(Parcel in) {
+	    // ! Important reading order from parcel
+	    GeoCache res = new GeoCache();
+	    res.id = in.readInt();
+	    res.name = in.readString();
+	    int[] location = new int[2];
+	    in.readIntArray(location);
+	    res.locationGeoPoint = new GeoPoint(location[0], location[1]);
+	    res.type = GeoCacheType.values()[in.readInt()];
+	    res.status = GeoCacheStatus.values()[in.readInt()];
+	    return res;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.os.Parcelable.Creator#newArray(int)
+	 */
+	public GeoCache[] newArray(int size) {
+	    return new GeoCache[size];
+	}
+    };
 }
