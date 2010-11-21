c04bc82e71a91488932934bfd04a89c7aa87d108
yuri.denison@gmail.com <yuri.denison@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-21 16:48:08 +0000

    update for SelectGeoCacheActivity

---
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/UserLocationOverlay.java
@@ -0,0 +1,43 @@
+package su.geocaching.android.ui.selectgeocache;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.location.Location;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import su.geocaching.android.utils.Helper;
+
+/**
+ * Author: Yuri Denison
+ * Date: 20.11.10 22:46
+ */
+public class UserLocationOverlay extends MyLocationOverlay {
+    private SelectGeoCacheMap context;
+    private MapView map;
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) {
+            return;
+        }
+
+        map.getController().animateTo(Helper.locationToGeoPoint(location));
+        map.getController().setCenter(Helper.locationToGeoPoint(location));
+
+        context.updateCacheOverlay(map.getProjection().fromPixels(0, 0),
+                map.getProjection().fromPixels(map.getWidth(), map.getHeight()));
+    }
+
+    public UserLocationOverlay(SelectGeoCacheMap arg0, MapView arg1) {
+        super(arg0, arg1);
+        context = arg0;
+        map = arg1;
+    }
+
+    @Override
+    protected void drawMyLocation(Canvas canvas, MapView mapView, Location lastFix, GeoPoint myLocation, long when) {
+        super.drawMyLocation(canvas, map, getLastFix(),
+                Helper.locationToGeoPoint(getLastFix()), when);
+    }
+}
