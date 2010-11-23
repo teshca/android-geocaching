295f80aee3d3b259fb98825e05d2d1f9cf85ce81
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-23 19:05:55 +0000

    rolled GeoCacheCompassManager

--- Android-Geocaching/src/su/geocaching/android/ui/geocachemap/GeoCacheCompassManager.java
+++ Android-Geocaching/src/su/geocaching/android/ui/geocachemap/GeoCacheCompassManager.java
@@ -12,7 +12,13 @@ import android.hardware.SensorManager;
  */
 public class GeoCacheCompassManager implements SensorEventListener {
 
+    private static final float RAD2DEG = (float) (180 / Math.PI);
     private SensorManager sensorManager;
+    private float[] afGravity = new float[3];
+    private float[] afGeomagnetic = new float[3];
+    private float[] afRotation = new float[16];
+    private float[] afInclination = new float[16];
+    private float[] afOrientation = new float[3];
     private ICompassAware context;
     private int lastAzimuth;
     private boolean isCompassAvailable;
@@ -27,25 +33,37 @@ public class GeoCacheCompassManager implements SensorEventListener {
 	this.context = context;
 	this.sensorManager = sensorManager;
 	isCompassAvailable = sensorManager != null;
-
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
+    /*
+     * Calculate new azimuth using ACCELEROMETER and MAGNETIC_FIELD
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
 	int type = event.sensor.getType();
-	int azimuth = 0;
+	float[] data;
+	if (type == Sensor.TYPE_ACCELEROMETER) {
+	    data = afGravity;
+	} else if (type == Sensor.TYPE_MAGNETIC_FIELD) {
+	    data = afGeomagnetic;
+	} else {
+	    // we do not handle this sensor type
+	    return;
+	}
 
-	if (type == Sensor.TYPE_ORIENTATION) {
-	    azimuth = (int) event.values[0];
+	for (int i = 0; i < 3; i++)
+	    data[i] = event.values[i];
 
-	    if (azimuth != lastAzimuth) {
-		lastAzimuth = azimuth;
-		context.updateAzimuth(lastAzimuth);
-	    }
+	SensorManager.getRotationMatrix(afRotation, afInclination, afGravity, afGeomagnetic);
+	SensorManager.getOrientation(afRotation, afOrientation);
+	int loclastAzimuth = (int) (afOrientation[0] * RAD2DEG);
+	if (loclastAzimuth != lastAzimuth) {
+	    lastAzimuth = loclastAzimuth;
+	    context.updateAzimuth(lastAzimuth);
 	}
     }
 
@@ -56,12 +74,14 @@ public class GeoCacheCompassManager implements SensorEventListener {
 	if (!isCompassAvailable) {
 	    return;
 	}
-	Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-	if (sensor == null) {
+	Sensor gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	Sensor magnitudeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+	if (gravitySensor == null || magnitudeSensor == null) {
 	    isCompassAvailable = false;
 	    return;
 	}
-	sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+	sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_UI);
+	sensorManager.registerListener(this, magnitudeSensor, SensorManager.SENSOR_DELAY_UI);
     }
 
     /**
