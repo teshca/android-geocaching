5e4e50587dc21044bfaa49fbc073b632a8f37fdf
grigory.kalabin <grigory.kalabin@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-11-27 19:50:52 +0000

    Added search last geocache function to "Search" button in dashboard

--- trunk/Android-Geocaching/src/su/geocaching/android/application/ApplicationMain.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/application/ApplicationMain.java
@@ -1,12 +1,15 @@
 package su.geocaching.android.application;
 
+import su.geocaching.android.model.datastorage.DbManager;
 import su.geocaching.android.model.datatype.GeoCache;
 import su.geocaching.android.ui.searchgeocache.GeoCacheCompassManager;
 import su.geocaching.android.ui.searchgeocache.GeoCacheLocationManager;
 import su.geocaching.android.ui.searchgeocache.GpsStatusListener;
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.hardware.SensorManager;
 import android.location.LocationManager;
+import android.util.Log;
 
 /**
  * Class which represent application. It can save common data of different
@@ -16,6 +19,9 @@ import android.location.LocationManager;
  * @Nov 20, 2010
  */
 public class ApplicationMain extends Application {
+
+    private static final String PREFS_NAME = "geocaching_prefs";
+    private static final String TAG = ApplicationMain.class.getCanonicalName();
 
     private GeoCache desiredGeoCache;
     private GeoCacheLocationManager locationManager;
@@ -31,12 +37,32 @@ public class ApplicationMain extends Application {
     public void onCreate() {
 	super.onCreate();
 
-	// debug stub
-	desiredGeoCache = new GeoCache(1);
+	// load last searched GeoCache
+	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	int desiredGeoCacheId = settings.getInt(GeoCache.class.getCanonicalName(), -1);
+	DbManager dbm = new DbManager(getApplicationContext());
+	dbm.openDB();
+	desiredGeoCache = dbm.getCacheByID(desiredGeoCacheId);
+	dbm.closeDB();
 
 	locationManager = new GeoCacheLocationManager((LocationManager) getSystemService(LOCATION_SERVICE));
 	compassManager = new GeoCacheCompassManager((SensorManager) getSystemService(SENSOR_SERVICE));
 	gpsStatusManager = new GpsStatusListener((LocationManager) getSystemService(LOCATION_SERVICE), getApplicationContext());
+	Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onTerminate() {
+	super.onTerminate();
+	Log.d(TAG, "onTerminate");
+	if (desiredGeoCache != null) {
+	    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	    SharedPreferences.Editor editor = settings.edit();
+	    editor.putInt(GeoCache.class.getCanonicalName(), desiredGeoCache.getId());
+	    // Commit the edits!
+	    editor.commit();
+	}
+
     }
 
     /**
@@ -45,8 +71,15 @@ public class ApplicationMain extends Application {
      *            past
      */
     public void setDesiredGeoCache(GeoCache desired) {
+	if (desiredGeoCache != null) {
+	    Log.d(TAG, "Save last searched geocache (id=" + Integer.toString(desiredGeoCache.getId()) + ") in settings");
+	    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	    SharedPreferences.Editor editor = settings.edit();
+	    editor.putInt(GeoCache.class.getCanonicalName(), desiredGeoCache.getId());
+	    // Commit the edits!
+	    editor.commit();
+	}
 	desiredGeoCache = desired;
-
     }
 
     /**
@@ -72,6 +105,10 @@ public class ApplicationMain extends Application {
 	return compassManager;
     }
 
+    /**
+     * @return gps status manager which can send to IGpsStatusAware updates of
+     *         status gps engine
+     */
     public GpsStatusListener getGpsStatusListener() {
 	return gpsStatusManager;
     }
