ab0b8c4f3c9d889e17ef77128e4d860a18a8a0db
yuri.denison <yuri.denison@gmail.com>
2010-11-28 21:52:49 +0000

    updated Timer logic

--- Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
+++ Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
@@ -17,13 +17,13 @@ public class MapUpdateTimer extends Timer{
     private static final int TOUCH_TIMER_DELAY = 0;
     private static final int TOUCH_TIMER_PERIOD = 500;
     private static final int MAP_STATE_TIMER_DELAY = 0;
-    private static final int MAP_STATE_TIMER_PERIOD = 700;
+    private static final int MAP_STATE_TIMER_PERIOD = 450;
 
     public MapUpdateTimer(SelectGeoCacheMap map) {
         State state = new State(map);
 
         this.schedule(new CheckRequestTimerTask(state), REQUEST_TIMER_DELAY, REQUEST_TIMER_PERIOD);
-        this.schedule(new CheckTouchTimerTask(state, map), TOUCH_TIMER_DELAY, TOUCH_TIMER_PERIOD);
+        //this.schedule(new CheckTouchTimerTask(state, map), TOUCH_TIMER_DELAY, TOUCH_TIMER_PERIOD);
         this.schedule(new CheckMapStateTimerTask(state, map, map.getCenter(), map.getZoom()),
                 MAP_STATE_TIMER_DELAY, MAP_STATE_TIMER_PERIOD);
     }
