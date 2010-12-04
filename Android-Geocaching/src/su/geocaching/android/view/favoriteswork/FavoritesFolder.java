81b05afd18f04ffca53353c349b74cac57b78f1a
vladimir.alekkseenko <vladimir.alekkseenko@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-12-04 12:51:43 +0000

    Optimized work with Database
    Add text field for type and status GeoCache
    
    Add string type and status GeoCache
    
    Delete menu and addRandomCache method

--- Android-Geocaching/src/su/geocaching/android/view/favoriteswork/FavoritesFolder.java
+++ Android-Geocaching/src/su/geocaching/android/view/favoriteswork/FavoritesFolder.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import su.geocaching.android.model.datastorage.DbManager;
 import su.geocaching.android.model.datatype.GeoCache;
+import su.geocaching.android.model.datatype.GeoCacheStatus;
+import su.geocaching.android.model.datatype.GeoCacheType;
 import su.geocaching.android.ui.R;
 
 import su.geocaching.android.view.showgeocacheinfo.ShowGeoCacheInfo;
@@ -40,7 +42,7 @@ public class FavoritesFolder extends Activity implements OnItemClickListener {
 	lvListShowCache = (ListView) findViewById(R.id.favorit_folder_listCach);
 	tvTitle = (TextView) findViewById(R.id.favorit_foldet_title_text);
 	dbm = new DbManager(getBaseContext());
-	dbm.openDB();
+	
     }
 
     private List<Map<String, ?>> createGeoCacheList(ArrayList<GeoCache> t) {
@@ -53,23 +55,44 @@ public class FavoritesFolder extends Activity implements OnItemClickListener {
 	    type[i] = t.get(i).getType().ordinal();
 	    name[i] = t.get(i).getName();
 
-	    switch (type[i]) {
+	    switch (t.get(i).getStatus().ordinal()){
+	    case 0:
+		map.put("statusText", getString(R.string.status_geocache_valid));
+		break;
 	    case 1:
-		map.put("type", R.drawable.icon_favorit_folder_traditional_cach);
+		map.put("statusText", getString(R.string.status_geocache_no_valid));
 		break;
 	    case 2:
+		map.put("statusText",getString(R.string.status_geocache_no_confirmed));
+		break;
+	   default:
+	       map.put("statusText", "???");
+	       break;
+	    }
+	    
+	    switch (type[i]) {
+	    case 0:
+		map.put("typeText", getString(R.string.type_geocache_traditional));
+		map.put("type", R.drawable.icon_favorit_folder_traditional_cach);
+		break;
+	    case 1:
+		map.put("typeText", getString(R.string.type_geocache_virtua));
 		map.put("type", R.drawable.icon_favorit_folder_virtual_cach);
 		break;
-	    case 3:
-		map.put("type", R.drawable.icon_favorites_folder_event);
+	    case 2:
+		map.put("typeText", getString(R.string.type_geocache_step_by_step));
+		map.put("type", R.drawable.icon_favorit_folder_step_by_step_cach);
 		break;
-	    case 4:
+	    case 3:
+		map.put("typeText",getString(R.string.type_geocache_event));
 		map.put("type", R.drawable.icon_favorit_folder_extrime_cach);
 		break;
-	    case 5:
+	    case 4:
+		map.put("typeText", getString(R.string.type_geocache_extreme));
 		map.put("type", R.drawable.icon_favorites_folder_event);
 		break;
 	    default:
+		map.put("typeText", "???");
 		map.put("type", R.drawable.icon_favorit_folder_traditional_cach);
 		break;
 	    }
@@ -81,36 +104,15 @@ public class FavoritesFolder extends Activity implements OnItemClickListener {
 	return ExitList;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.favorites_folder_menu, menu);
-	return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-	if (item.getItemId() == R.id.favorit_folder_Add_Cache_Button) {
-	    addRandomCache();
-	    return true;
-	}
-	return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStop() {
-	dbm.closeDB();
-	super.onStop();
-    }
 
     @Override
     protected void onStart() {
-	
+	dbm.openDB();
 	mass = dbm.getArrayGeoCache();
-	
+	dbm.closeDB();
 	if (mass != null) {
-	    SimpleAdapter ap = new SimpleAdapter(this, createGeoCacheList(mass), R.layout.row_in_favorit_rolder, new String[] { "type", "name" }, new int[] { R.id.favorit_list_imagebutton_type,
-		R.id.favorit_list_textview_name });
+	    SimpleAdapter ap = new SimpleAdapter(this, createGeoCacheList(mass), R.layout.row_in_favorit_rolder, new String[] { "type", "name", "typeText", "statusText" }, new int[] {
+		    R.id.favorit_list_imagebutton_type, R.id.favorit_list_textview_name, R.id.favorites_row_type_text, R.id.favorites_row_statys_text });
 	    lvListShowCache.setAdapter(ap);
 	    lvListShowCache.setOnItemClickListener(this);
 	} else {
@@ -121,12 +123,7 @@ public class FavoritesFolder extends Activity implements OnItemClickListener {
 	super.onStart();
     }
 
-    @Override
-    protected void onRestart() {
-	dbm.openDB();
-        super.onRestart();
-    }
-    
+
     @Override
     public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 	Intent intent = new Intent(this, ShowGeoCacheInfo.class);
@@ -134,26 +131,4 @@ public class FavoritesFolder extends Activity implements OnItemClickListener {
 	startActivity(intent);
     }
 
-    private void addRandomCache() {
-	dbm.insert2_for_testing(234, "Name1", 1, 2, 5442387, -2030774, "My Web text-1");
-	dbm.insert2_for_testing(239, "Name2", 2, 2, 5626802, -3454912, "My Web text-2");
-	dbm.insert2_for_testing(232, "Name3", 1, 2, 556918, -3738894, "My Web text 3");
-	dbm.insert2_for_testing(223, "Name4", 1, 2, 5745883, -2933664, "My Web text 4");
-	dbm.insert2_for_testing(124, "Name5", 3, 2, 4850787, -4336517, "My Web text 5");
-	dbm.insert2_for_testing(322, "Name6", 1, 2, 5531899, -3659884, "My Web text 6");
-	dbm.insert2_for_testing(17, "Name7", 4, 2, 5619838, -3757061, "My Web text 7");
-	dbm.insert2_for_testing(43, "Name8", 2, 2, 573722, -2854325, "My Web text 8");
-	dbm.insert2_for_testing(412, "Name9", 2, 2, 4425389, -3355556, "My Web text 9");
-	dbm.insert2_for_testing(430, "Name10", 2, 2, 5716796, -3647061, "My Web text 10");
-	dbm.insert2_for_testing(432, "Name11", 3, 2, 5434437, -8321288, "My Web text 11");
-	dbm.insert2_for_testing(439, "Name12", 1, 2, 5524446, -3353614, "My Web text 12");
-
-	mass = dbm.getArrayGeoCache();
-
-	SimpleAdapter ap = new SimpleAdapter(this, createGeoCacheList(mass), R.layout.row_in_favorit_rolder, new String[] { "type", "name" }, new int[] { R.id.favorit_list_imagebutton_type,
-		R.id.favorit_list_textview_name });
-	lvListShowCache.setAdapter(ap);
-	lvListShowCache.setOnItemClickListener(this);
-
-    }
 }
