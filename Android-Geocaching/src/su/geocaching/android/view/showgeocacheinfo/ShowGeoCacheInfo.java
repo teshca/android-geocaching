10fa9333358bc0e2c54d4016c124a476f7509c9b
vladimir.alekkseenko <vladimir.alekkseenko@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-12-04 12:48:57 +0000

    Change work method getWebText.
    Add IInternetAware interface
    
    Optimized work with Database.
    
    Added text fields for type and status GeoCache.

--- Android-Geocaching/src/su/geocaching/android/view/showgeocacheinfo/ShowGeoCacheInfo.java
+++ Android-Geocaching/src/su/geocaching/android/view/showgeocacheinfo/ShowGeoCacheInfo.java
@@ -4,169 +4,229 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.Charset;
 
+import su.geocaching.android.ui.geocachemap.ConnectionManager;
+import su.geocaching.android.ui.geocachemap.IInternetAware;
+import su.geocaching.android.controller.Controller;
 import su.geocaching.android.model.datastorage.DbManager;
 import su.geocaching.android.model.datatype.GeoCache;
 import su.geocaching.android.ui.R;
 import su.geocaching.android.ui.searchgeocache.SearchGeoCacheMap;
 import android.app.Activity;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageButton;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
-public class ShowGeoCacheInfo extends Activity implements OnCheckedChangeListener, OnClickListener {
-    private static final String TAG = "ShowGeoCacheInfo";
-    private WebView wvWebrouse;
-    private TextView tvNametext;
-    private Display dsDis;
+public class ShowGeoCacheInfo extends Activity implements OnCheckedChangeListener, OnClickListener, IInternetAware{
+    private static final String TAG = ShowGeoCacheInfo.class.getCanonicalName();
+    private WebView webView;
+    private TextView tvNameText;
+    private TextView tvTypeGeoCacheText;
+    private TextView tvStatusGeoCacheText;
     private ImageButton btGo;
     private CheckBox cbAddDelCache;
     private DbManager dbm = null;
-    private NetworkInfo wifi;
-    private GeoCache cache;
-    private String htmlTextGeoCache = "default text";
-    private boolean haveCache;
+    private GeoCache GeoCacheForShowInfo;
+    private String htmlTextGeoCache;
+    private boolean isCacheStoredInDataBase;
+    private ConnectionManager connectManag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.info_geocach_activity);
 
-	ConnectivityManager network = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-	wifi = network.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 
 	if (dbm == null) {
-	    dbm = new DbManager(getBaseContext());
+	    dbm = new DbManager(getApplicationContext());
 	}
 
-	wvWebrouse = (WebView) findViewById(R.id.info_web_brouse);
+	webView = (WebView) findViewById(R.id.info_web_brouse);
 	btGo = (ImageButton) findViewById(R.id.info_geocach_Go_button);
 	cbAddDelCache = (CheckBox) findViewById(R.id.info_geocache_add_del);
-
-	tvNametext = (TextView) findViewById(R.id.info_text_name);
-	dsDis = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
-	wvWebrouse.setLayoutParams(new LayoutParams(dsDis.getWidth(), dsDis.getHeight()));
-
-	cbAddDelCache.setOnCheckedChangeListener(this);
-	btGo.setOnClickListener(this);
-
-	wvWebrouse.setWebViewClient(new WVclient());
+	
+	tvNameText = (TextView) findViewById(R.id.info_text_name);
+	tvTypeGeoCacheText=(TextView)findViewById(R.id.info_GeoCache_type);
+	tvStatusGeoCacheText=(TextView)findViewById(R.id.info_GeoCache_status);
+	webView.getSettings().setJavaScriptEnabled(true);
+	webView.setWebViewClient( new MyWebClient());
+	
     }
 
-    private class WVclient extends WebViewClient {
-	@Override
-	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	    view.loadUrl(url);
-	    return true;
-	}
-    }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 	if (isChecked) {
-	    if (!haveCache) dbm.addGeoCache(cache, htmlTextGeoCache);
+		dbm.openDB();
+		dbm.addGeoCache(GeoCacheForShowInfo, htmlTextGeoCache);
+		dbm.closeDB();
 	} else {
-	    dbm.deleteCacheById(cache.getId());
+	    dbm.openDB();
+	    dbm.deleteCacheById(GeoCacheForShowInfo.getId());
+	    dbm.closeDB();
 	}
 
     }
 
     @Override
     protected void onStop() {
-	dbm.closeDB();
-	super.onStop();
+        connectManag.removeSubscriber(this);
+        super.onStop();
     }
-
+   
     @Override
     protected void onStart() {
+	GeoCacheForShowInfo = getIntent().getParcelableExtra(GeoCache.class.getCanonicalName());
+	
+	connectManag=Controller.getInstance().getConnectionManager(this);
+	connectManag.addSubscriber(this);
+	
 	dbm.openDB();
-	cache = getIntent().getParcelableExtra(GeoCache.class.getCanonicalName());
-
-	if (dbm.getCacheByID(cache.getId()) == null) {
-	    haveCache = false;
-	} else {
-	    haveCache = true;
-	}
-
-	if (haveCache) {
+	isCacheStoredInDataBase=(dbm.getCacheByID(GeoCacheForShowInfo.getId()) != null);
+	dbm.closeDB();
+	
+	
+	
+	if (isCacheStoredInDataBase) {
 	    Log.d(TAG, "Cache in BD");
 	    cbAddDelCache.setChecked(true);
-	    htmlTextGeoCache = dbm.getWebTextById(cache.getId());
-	    Log.d("Cache in BD html text:", htmlTextGeoCache);
+	    dbm.openDB();
+	    htmlTextGeoCache = dbm.getWebTextById(GeoCacheForShowInfo.getId());
+	    dbm.closeDB();
 	} else {
 	    Log.d(TAG, "Cache NOT BD");
 	    try {
-		htmlTextGeoCache = getWebText(cache.getId());
+		htmlTextGeoCache = getWebText(GeoCacheForShowInfo.getId());
 		Log.d(TAG, htmlTextGeoCache);
 	    } catch (IOException e) {
 		Log.d(TAG + "getWebText", "IOExeption :" + e.toString());
 		e.printStackTrace();
 	    }
 	}
+
+	tvNameText.setText(GeoCacheForShowInfo.getName());
+	
 	
 
-	tvNametext.setText(cache.getName());
-
-	if (!wifi.isAvailable()) {
-	    if (!haveCache) {
+	    switch (GeoCacheForShowInfo.getStatus().ordinal()){
+	    case 0:
+		tvStatusGeoCacheText.setText(getString(R.string.status_geocache_valid));
+		break;
+	    case 1:
+		tvStatusGeoCacheText.setText(getString(R.string.status_geocache_no_valid));
+		break;
+	    case 2:
+		tvStatusGeoCacheText.setText(getString(R.string.status_geocache_no_confirmed));
+		break;
+	   default:
+	       tvStatusGeoCacheText.setText(getString(R.string.status_geocache_no_confirmed));
+	       break;
+	    }
+	    
+	    switch (GeoCacheForShowInfo.getType().ordinal()) {
+	    case 0:
+		tvTypeGeoCacheText.setText(getString(R.string.type_geocache_traditional));
+		break;
+	    case 1:
+		tvTypeGeoCacheText.setText(getString(R.string.type_geocache_virtua));
+		break;
+	    case 2:
+		tvTypeGeoCacheText.setText(getString(R.string.type_geocache_step_by_step));
+		break;
+	    case 3:
+		tvTypeGeoCacheText.setText(getString(R.string.type_geocache_event));
+		break;
+	    case 4:
+		tvTypeGeoCacheText.setText(getString(R.string.type_geocache_extreme));
+		break;
+	    default:
+		tvTypeGeoCacheText.setText("???");
+		break;
+	    }
+	
+	
+	cbAddDelCache.setOnCheckedChangeListener(this);
+	btGo.setOnClickListener(this);
+	
+	if (! connectManag.isInternetConnected()) {
+	    if (!isCacheStoredInDataBase) {
 		Log.d("Info Cache", "Internet - NOT + BD NOT have cache");
-		wvWebrouse.loadData("<?xml version='1.0' encoding='utf-8'?>" + "<center>" + getString(R.string.info_geocach_not_internet_and_not_in_DB) + "</center>", "text/html", "utf-8");
+		webView.loadData("<?xml version='1.0' encoding='utf-8'?>" + "<center>" + getString(R.string.info_geocach_not_internet_and_not_in_DB) + "</center>", "text/html", "utf-8");
 	    } else {
 		Log.d("Info Cache", "Internet - NOT + BD HAVE cache");
-		wvWebrouse.loadData("<?xml version='1.0' encoding='utf-8'?>" + htmlTextGeoCache, "text/html", "utf-8");
+		webView.loadData( htmlTextGeoCache, "text/html", "utf-8");
 	    }
 
 	} else {
-	    if (!haveCache) {
+	    if (!isCacheStoredInDataBase) {
 		Log.d("Info Cache", "Internet - YES + BD NOT have cache");
-		wvWebrouse.loadData("<?xml version='1.0' encoding='utf-8'?>" + htmlTextGeoCache, "text/html", "utf-8");
+		webView.loadData(htmlTextGeoCache, "text/html", "utf-8");
 	    } else {
 		Log.d("Info Cache", "Internet - YES + BD HAVE cache");
-		wvWebrouse.loadData("<?xml version='1.0' encoding='utf-8'?>" +htmlTextGeoCache, "text/html", "utf-8");
-
+		webView.loadData( htmlTextGeoCache, "text/html", "");
 	    }
 
 	}
 
+	
+	
 	super.onStart();
     }
 
     @Override
     public void onClick(View v) {
 	Intent intent = new Intent(this, SearchGeoCacheMap.class);
-	intent.putExtra(GeoCache.class.getCanonicalName(), cache);
+	intent.putExtra(GeoCache.class.getCanonicalName(), GeoCacheForShowInfo);
 	startActivity(intent);
     }
 
     private String getWebText(int id) throws IOException {
-	String html = "";
+	StringBuilder html = new StringBuilder();
 	String html2 = "";
-	int i = 0;
 	URL url = new URL("http://pda.geocaching.su/cache.php?cid=" + id);
-	BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), Charset.forName("CP1251")));
-	while ((html2 = in.readLine()) != null) {
-	    if (i != 2) {
-		html += html2;
-	    }
-	    i++;
-	}
+	BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "windows-1251"));
 	
-	return html;
+	html.append(in.readLine());
+	html.append(in.readLine());
+	html.append(in.readLine().replace("windows-1251", "utf-8"));
+	
+	while ((html2 = in.readLine()) != null) {
+	    html.append(html2);
+	}
 
+	return html.toString();
+
+    }
+
+
+    private class MyWebClient extends WebViewClient {
+	    @Override
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	        view.loadUrl(url);
+	        return true;
+	    }
+	}
+    
+    
+    @Override
+    public void onInternetLost() {
+	btGo.setOnClickListener(null);
+	Toast.makeText(getBaseContext(), getString(R.string.select_geocache_status_without_internet), Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onInternetFound() {
+	btGo.setOnClickListener(this);
     }
 }
