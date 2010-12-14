c36d498f08c1f7144908254748f378eeaf22bbbc
yuri.denison <yuri.denison@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2010-12-14 18:56:20 +0000

    added progress bar optimized adding geocaches to overlay added check if itemizedoverlay already consists geocache updated Timer codestyle timer delay set up to 1000ms (because of draw optimization) added limit to draw geocaches (if more than 500, toast is shown)

--- trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
@@ -12,17 +12,14 @@ import java.util.Timer;
  */
 public class MapUpdateTimer extends Timer{
     private static final int REQUEST_TIMER_DELAY = 100;
-    private static final int REQUEST_TIMER_PERIOD = 5000;
-    private static final int TOUCH_TIMER_DELAY = 0;
-    private static final int TOUCH_TIMER_PERIOD = 500;
+    private static final int REQUEST_TIMER_PERIOD = 1000;
     private static final int MAP_STATE_TIMER_DELAY = 0;
-    private static final int MAP_STATE_TIMER_PERIOD = 2000;
+    private static final int MAP_STATE_TIMER_PERIOD = 1000;
 
     public MapUpdateTimer(SelectGeoCacheMap map) {
         State state = new State(map);
 
         this.schedule(new CheckRequestTimerTask(state), REQUEST_TIMER_DELAY, REQUEST_TIMER_PERIOD);
-        //this.schedule(new CheckTouchTimerTask(state, map), TOUCH_TIMER_DELAY, TOUCH_TIMER_PERIOD);
         this.schedule(new CheckMapStateTimerTask(state, map, map.getCenter(), map.getZoom()),
                 MAP_STATE_TIMER_DELAY, MAP_STATE_TIMER_PERIOD);
     }
