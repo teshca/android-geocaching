dfdb2afcdf8da3373665694dced8bdec746a6dc5
yuri.denison <yuri.denison@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-02-17 19:52:51 +0000

    added code of group cache

---
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/geocachegroup/Pair.java
@@ -0,0 +1,16 @@
+package su.geocaching.android.ui.selectgeocache.geocachegroup;
+
+/**
+ * @author Yuri Denison; yuri.denison@gmail.com
+ * @since 17.02.11
+ */
+
+public class Pair {
+    public int x;
+    public int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
