8b17a95d2bc8006ee2e201961fa16922ab3866ed
vova.filatov <vova.filatov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-02-18 19:13:50 +0000

    added GoogleAnalytics in all activity

--- Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
+++ Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
@@ -7,11 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 public class AboutActivity extends Activity implements OnClickListener {
 
     private Button exitButton;
-
+    private GoogleAnalyticsTracker tracker;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -19,11 +19,15 @@ public class AboutActivity extends Activity implements OnClickListener {
 	setContentView(R.layout.about);
 	exitButton = (Button) findViewById(R.id.about_exit_button);
 	exitButton.setOnClickListener(this);
+	tracker = GoogleAnalyticsTracker.getInstance();
+	tracker.start("UA-20327116-1", this);
+	tracker.trackPageView("/aboutActivity");
     }
 
     @Override
     public void onClick(View v) {
 	if (v.equals(exitButton)) {
+		tracker.stop();
 	    super.finish();
 	    Log.d("aboutActivity", "exit");
 	}
