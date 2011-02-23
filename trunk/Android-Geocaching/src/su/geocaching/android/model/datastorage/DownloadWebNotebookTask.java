d282b91931644ddba261baae8f2c4bf8e98efc76
vova.filatov <vova.filatov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-02-23 18:26:02 +0000

    added web_notebook, but favorites has bugs.

---
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/datastorage/DownloadWebNotebookTask.java
@@ -0,0 +1,68 @@
+package su.geocaching.android.model.datastorage;
+
+import su.geocaching.android.model.datastorage.DbManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import android.text.Html;
+import android.webkit.WebView;
+import su.geocaching.android.model.datatype.GeoCache;
+import android.os.AsyncTask;
+
+public class DownloadWebNotebookTask extends AsyncTask<String, Void, String> {
+	private DbManager dbManager;
+	private boolean isCacheStoredInDataBase;
+	private int idCache;
+	private String htmlNotebookTextGeoCache = "";
+
+	public DownloadWebNotebookTask(DbManager db, boolean isCacheStoredInDataBase, int idCache) {
+		this.dbManager = db;
+		this.isCacheStoredInDataBase = isCacheStoredInDataBase;
+		this.idCache = idCache;
+	}
+
+	@Override
+	protected String doInBackground(String... params) {
+		if (params[0] == null || params[0] == "") {
+			if (isCacheStoredInDataBase) {
+				dbManager.openDB();
+				htmlNotebookTextGeoCache = dbManager.getWebNotebookTextById(idCache);
+				dbManager.closeDB();
+			}
+			if (htmlNotebookTextGeoCache == null || htmlNotebookTextGeoCache == "") {
+				try {
+					htmlNotebookTextGeoCache = getWebText(idCache);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return htmlNotebookTextGeoCache;
+		} else {
+			return params[0];
+		}
+
+	}
+
+	private String getWebText(int id) throws IOException {
+		StringBuilder html = new StringBuilder();
+		String html2 = "";
+		URL url = new URL("http://pda.geocaching.su/note.php?cid=" + id + "&mode=0");
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "windows-1251"));
+
+		html.append(in.readLine());
+		html.append(in.readLine());
+		html.append(in.readLine().replace("windows-1251", "utf-8"));
+
+		while ((html2 = in.readLine()) != null) {
+			html.append(html2);
+		}
+
+		return html.toString();
+
+	}
+}
