367794a2817750b42b947d825dd65ddb890c7520
yuri.denison <yuri.denison@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-02-24 01:28:16 +0000

    update settings to SelectGeoCacheMap

--- trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/selectgeocache/timer/MapUpdateTimer.java
@@ -12,15 +12,24 @@ import java.util.Timer;
  */
 public class MapUpdateTimer extends Timer {
     private static final int REQUEST_TIMER_DELAY = 50;
-    private static final int REQUEST_TIMER_PERIOD = 400;
+    private int REQUEST_TIMER_PERIOD = 400;
     private static final int MAP_STATE_TIMER_DELAY = 0;
     private static final int MAP_STATE_TIMER_PERIOD = 400;
 
     public MapUpdateTimer(SelectGeoCacheMap map) {
         State state = new State(map);
+        scheduleTasks(map, state);
+    }
 
+    public MapUpdateTimer(SelectGeoCacheMap map, int requestPeriod) {
+        State state = new State(map);
+        this.REQUEST_TIMER_PERIOD = requestPeriod;
+        scheduleTasks(map, state);
+    }
+
+    private void scheduleTasks(SelectGeoCacheMap map, State state) {
         this.schedule(new CheckRequestTimerTask(state), REQUEST_TIMER_DELAY, REQUEST_TIMER_PERIOD);
         this.schedule(new CheckMapStateTimerTask(state, map, map.getCenter(), map.getZoom()),
-                MAP_STATE_TIMER_DELAY, MAP_STATE_TIMER_PERIOD);
+            MAP_STATE_TIMER_DELAY, MAP_STATE_TIMER_PERIOD);
     }
 }
