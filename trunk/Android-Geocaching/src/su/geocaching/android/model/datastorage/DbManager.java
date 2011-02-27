2652b393ac5e8640f22f27142d33b4544c446573
vladimir.alekkseenko@gmail.com <vladimir.alekkseenko@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-02-27 19:17:08 +0000

    Now databese must add column for geocache notebook.

--- trunk/Android-Geocaching/src/su/geocaching/android/model/datastorage/DbManager.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/datastorage/DbManager.java
@@ -54,6 +54,9 @@ public class DbManager extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion<newVersion){
+            db.execSQL(String.format("ALTER TABLE %s ADD %s string;",DATABASE_NAME_TABLE,COLUMN_NOTEBOOK_TEXT));
+        }
 	}
 
 	/**
