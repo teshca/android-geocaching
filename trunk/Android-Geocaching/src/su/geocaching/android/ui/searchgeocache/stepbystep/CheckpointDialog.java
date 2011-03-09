c01c516e56fe18f79433c836e414eef045ad9db7
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-03-09 11:24:12 +0000

    separate SearchCacheOverlay and CheckpointCacheOverlay

--- trunk/Android-Geocaching/src/su/geocaching/android/ui/searchgeocache/stepbystep/CheckpointDialog.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/searchgeocache/stepbystep/CheckpointDialog.java
@@ -1,31 +1,32 @@
 package su.geocaching.android.ui.searchgeocache.stepbystep;
 
+import su.geocaching.android.controller.Controller;
+import su.geocaching.android.ui.R;
+import su.geocaching.android.ui.geocachemap.CheckpointCacheOverlay;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import com.google.android.maps.MapView;
-import su.geocaching.android.controller.Controller;
-import su.geocaching.android.ui.R;
-import su.geocaching.android.ui.geocachemap.SearchCacheOverlay;
 
 public class CheckpointDialog extends Dialog {
 
     private Button active, delete;
 
-    private SearchCacheOverlay searchOverlay;
+    private CheckpointCacheOverlay searchOverlay;
     private MapView map;
     private int index;
 
-    public CheckpointDialog(Context context, int index, SearchCacheOverlay searchOverlay, MapView map) {
+    public CheckpointDialog(Context context, int index, CheckpointCacheOverlay checkpointCacheOverlay, MapView map) {
         super(context);
 
         String title = Controller.getInstance().getResourceManager().getString(R.string.checkpoint_dialog_title) + " " + index;
         setTitle(title);
         // this.context = context;
         this.index = index;
-        this.searchOverlay = searchOverlay;
+        this.searchOverlay = checkpointCacheOverlay;
         this.map = map;
     }
 
@@ -54,5 +55,4 @@ public class CheckpointDialog extends Dialog {
             dismiss();
         }
     }
-
 }
