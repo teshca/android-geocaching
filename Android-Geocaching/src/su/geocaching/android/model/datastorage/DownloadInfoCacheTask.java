aab4194f38b16cb2ce1d7f332a11ce27fb8c6368
vova.filatov <vova.filatov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-03-13 16:00:27 +0000

    added progressDialog and customDialog in ShowGeoCacheInfo.java

--- Android-Geocaching/src/su/geocaching/android/model/datastorage/DownloadInfoCacheTask.java
+++ Android-Geocaching/src/su/geocaching/android/model/datastorage/DownloadInfoCacheTask.java
@@ -1,23 +1,41 @@
 package su.geocaching.android.model.datastorage;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.webkit.WebView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import su.geocaching.android.controller.Controller;
+
 public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
+    private static final String HTTP_PDA_GEOCACHING_SU = "http://pda.geocaching.su/";
+    
     private DbManager dbManager;
     private boolean isCacheStoredInDataBase;
+    private ProgressDialog prDialog;
     private int idCache;
-
-    public DownloadInfoCacheTask(DbManager db, boolean isCacheStoredInDataBase, int idCache) {
+    private Context context;
+	private WebView webView;
+    public DownloadInfoCacheTask(DbManager db, boolean isCacheStoredInDataBase, int idCache, Context context, WebView web) {
         this.dbManager = db;
         this.isCacheStoredInDataBase = isCacheStoredInDataBase;
         this.idCache = idCache;
+        this.context = context;
+        this.webView = web;
+     //   this. prDialog = new ProgressDialog(context);
+         
     }
-
+    @Override
+    protected void onPreExecute() {
+      prDialog = new ProgressDialog(context);
+      prDialog.setMessage("Скачивание информации");
+      prDialog.show();
+    };
     @Override
     protected String doInBackground(String... params) {
         if (params[0] == null || params[0] == "") {
@@ -33,6 +51,7 @@ public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
                     e.printStackTrace();
                 }
         }
+        webView.loadDataWithBaseURL(HTTP_PDA_GEOCACHING_SU, params[0], "text/html", "utf-8", "");
         return params[0];
     }
 
@@ -51,6 +70,11 @@ public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
         }
 
         return html.toString();
-
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        // TODO Auto-generated method stub
+        prDialog.dismiss();
+        super.onPostExecute(result);
     }
 }
