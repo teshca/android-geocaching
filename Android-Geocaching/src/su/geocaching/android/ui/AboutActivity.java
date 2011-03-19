8df868e0e886ce2dc8064784f46d66000f6960b6
Anatoliy.Nyatin <Anatoliy.Nyatin@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-03-19 18:12:16 +0000

    add default values to preferences. update app version in AboutActivity

--- Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
+++ Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
@@ -1,6 +1,8 @@
 package su.geocaching.android.ui;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,16 +14,23 @@ public class AboutActivity extends Activity {
     private static final String TAG = AboutActivity.class.getCanonicalName();
 
     private GoogleAnalyticsTracker tracker;
-    private TextView tv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LogManager.d(TAG, "on create");
         setContentView(R.layout.about);
-        tv = (TextView) findViewById(R.id.about_content);
+        
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            String aboutTitle = "Геокешинг версия " + versionName;
+            this.setTitle((CharSequence) aboutTitle);
+        } catch (NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
         tracker = GoogleAnalyticsTracker.getInstance();
-
         tracker.start(getString(R.string.id_Google_Analytics), this);
         tracker.trackPageView(getString(R.string.about_activity_folder));
         tracker.dispatch();
