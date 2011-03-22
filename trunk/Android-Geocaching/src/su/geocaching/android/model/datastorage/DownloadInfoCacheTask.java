2ffeee661e8e89cdf947e8ec0145d3b48f94bbb9
yuri.denison <yuri.denison@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-03-22 22:46:01 +0000

    update code use inspection "UnnecessarySemicolon"

--- trunk/Android-Geocaching/src/su/geocaching/android/model/datastorage/DownloadInfoCacheTask.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/model/datastorage/DownloadInfoCacheTask.java
@@ -3,37 +3,38 @@ package su.geocaching.android.model.datastorage;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.webkit.WebView;
+import su.geocaching.android.ui.R;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import su.geocaching.android.ui.R;
-
 public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
-    
+
     private DbManager dbManager;
     private boolean isCacheStoredInDataBase;
     private ProgressDialog prDialog;
     private int idCache;
     private Context context;
-	public DownloadInfoCacheTask(DbManager db, boolean isCacheStoredInDataBase, int idCache, Context context) {
+
+    public DownloadInfoCacheTask(DbManager db, boolean isCacheStoredInDataBase, int idCache, Context context) {
         this.dbManager = db;
         this.isCacheStoredInDataBase = isCacheStoredInDataBase;
         this.idCache = idCache;
-        this.context = context;     
+        this.context = context;
     }
+
     @Override
     protected void onPreExecute() {
-      prDialog = new ProgressDialog(context);
-      prDialog.setMessage(context.getString(R.string.download_info));
-      prDialog.show();
-    };
+        prDialog = new ProgressDialog(context);
+        prDialog.setMessage(context.getString(R.string.download_info));
+        prDialog.show();
+    }
+
     @Override
     protected String doInBackground(String... params) {
-        if (params[0] == null || params[0] == "") {
+        if (params[0] == null || params[0].equals("")) {
             if (isCacheStoredInDataBase) {
                 params[0] = dbManager.getWebTextById(idCache);
             } else
@@ -49,7 +50,7 @@ public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
 
     private String getWebText(int id) throws IOException {
         StringBuilder html = new StringBuilder();
-        String html2 = "";
+        String html2;
         URL url = new URL("http://pda.geocaching.su/cache.php?cid=" + id + "&mode=0");
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), "windows-1251"));
 
@@ -63,6 +64,7 @@ public class DownloadInfoCacheTask extends AsyncTask<String, Integer, String> {
 
         return html.toString();
     }
+
     @Override
     protected void onPostExecute(String result) {
         // TODO Auto-generated method stub
