2befc8a87b213282d59bd70469e32cb761c14dd0
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-03-30 18:38:20 +0000

    little change of appearance of CheckpointDialog

--- Android-Geocaching/src/su/geocaching/android/ui/searchgeocache/stepbystep/CheckpointDialog.java
+++ Android-Geocaching/src/su/geocaching/android/ui/searchgeocache/stepbystep/CheckpointDialog.java
@@ -2,6 +2,7 @@ package su.geocaching.android.ui.searchgeocache.stepbystep;
 
 import su.geocaching.android.controller.CheckpointManager;
 import su.geocaching.android.controller.Controller;
+import su.geocaching.android.controller.ResourceManager;
 import su.geocaching.android.controller.UiHelper;
 import su.geocaching.android.ui.R;
 import su.geocaching.android.utils.GpsHelper;
@@ -9,13 +10,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class CheckpointDialog extends Activity {
-
-    private Button active, delete;
-    private TextView coordinates;
 
     private CheckpointManager checkpointManager;
     private int checkpointId;
@@ -25,29 +22,27 @@ public class CheckpointDialog extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkpoint_dialog);
 
-        coordinates = (TextView) findViewById(R.id.checkpointCoordinate);
-        active = (Button) findViewById(R.id.checkpointActiveButton);
-        delete = (Button) findViewById(R.id.checkpointDeleteButton);
-        ButtonClickListener clickListener = new ButtonClickListener();
-        active.setOnClickListener(clickListener);
-        delete.setOnClickListener(clickListener);
-
         Intent intent = getIntent();
         checkpointId = intent.getIntExtra(UiHelper.CACHE_ID, 0);
         checkpointManager = Controller.getInstance().getCheckpointManager(Controller.getInstance().getPreferencesManager().getLastSearchedGeoCache().getId());
+        ResourceManager rm = Controller.getInstance().getResourceManager();
+
+        TextView coordinates = (TextView) findViewById(R.id.checkpointCoordinate);
+        TextView status = (TextView) findViewById(R.id.tvCheckpointDialogStatus);
+
         coordinates.setText(GpsHelper.coordinateToString(checkpointManager.getGeoCache(checkpointId).getLocationGeoPoint()));
+        status.setText(rm.getGeoCacheStatus(checkpointManager.getGeoCache(checkpointId)));
+
+        setTitle(getString(R.string.checkpoint_dialog_title)+" " + checkpointId);
     }
 
-    private class ButtonClickListener implements android.view.View.OnClickListener {
+    public void onActiveClick(View v) {
+        checkpointManager.setActiveItem(checkpointId);
+        finish();
+    }
 
-        @Override
-        public void onClick(View v) {
-            if (v.equals(active)) {
-                checkpointManager.setActiveItem(checkpointId);
-            } else if (v.equals(delete)) {
-                checkpointManager.removeCheckpoint(checkpointId);
-            }
-            finish();
-        }
+    public void onDeleteClick(View v) {
+        checkpointManager.removeCheckpoint(checkpointId);
+        finish();
     }
 }
