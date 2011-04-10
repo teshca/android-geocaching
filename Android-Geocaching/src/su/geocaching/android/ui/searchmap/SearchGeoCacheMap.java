5ddd524cf803f408f8bb387188c56352719d8260
grigory.kalabin <grigory.kalabin@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-04-10 16:09:01 +0000

    added reading compassSpeed from preferences in search geocache map

--- Android-Geocaching/src/su/geocaching/android/ui/searchmap/SearchGeoCacheMap.java
+++ Android-Geocaching/src/su/geocaching/android/ui/searchmap/SearchGeoCacheMap.java
@@ -17,6 +17,7 @@ import su.geocaching.android.controller.IInternetAware;
 import su.geocaching.android.controller.ILocationAware;
 import su.geocaching.android.controller.LogManager;
 import su.geocaching.android.controller.UiHelper;
+import su.geocaching.android.controller.compass.CompassSpeed;
 import su.geocaching.android.controller.compass.SmoothCompassThread;
 import su.geocaching.android.model.datatype.GeoCache;
 import su.geocaching.android.model.datatype.GeoCacheStatus;
@@ -584,6 +585,7 @@ public class SearchGeoCacheMap extends MapActivity implements IInternetAware, IL
         if (animationThread == null) {
             animationThread = new SmoothCompassThread(userOverlay);
             animationThread.setRunning(true);
+            animationThread.setSpeed(CompassSpeed.valueOf(mController.getPreferencesManager().getCompassSpeed()));
             animationThread.start();
         }
     }
