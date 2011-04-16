b8deae107bba2881091e6e30bcab25e4f2030c43
vova.filatov <vova.filatov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-04-16 21:13:42 +0000

    added "delete all GeoCaches in database" in menu favorites

--- Android-Geocaching/src/su/geocaching/android/ui/FavoritesFolder.java
+++ Android-Geocaching/src/su/geocaching/android/ui/FavoritesFolder.java
@@ -3,8 +3,14 @@ package su.geocaching.android.ui;
 import su.geocaching.android.controller.Controller;
 import su.geocaching.android.controller.LogManager;
 import su.geocaching.android.model.datatype.GeoCache;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
@@ -36,6 +42,7 @@ public class FavoritesFolder extends AbstractCacheFolder {
                     R.id.favorite_list_text_view_name, R.id.favorites_row_type_text, R.id.favorites_row_status_text });
             lvListShowCache.setAdapter(simpleAdapter);
         }
+       
         super.onResume();
     }
 
@@ -45,4 +52,52 @@ public class FavoritesFolder extends AbstractCacheFolder {
         intent.putExtra(GeoCache.class.getCanonicalName(), favoritesList.get(arg2));
         startActivity(intent);
     }
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (favoritesList.isEmpty()){
+            menu.getItem(0).setEnabled(false);
+        }
+        else{
+            menu.getItem(0).setEnabled(true);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.favorites_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.delete_all_cache_in_database:
+            createAlertDialog();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    private void createAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(this.getString(R.string.ask_delet_all_cache_in_database)).setPositiveButton(this.getString(R.string.yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dbm.clearDB();
+                onResume();
+                dialog.cancel();
+            }
+        }).setNegativeButton(this.getString(R.string.no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+                finish();
+            }
+        });
+        AlertDialog turnOnInternetAlert = builder.create();
+        turnOnInternetAlert.show();
+    }
+    
 }
