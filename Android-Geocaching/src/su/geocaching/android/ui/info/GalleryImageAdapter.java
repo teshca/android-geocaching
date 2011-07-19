80cb0fca2099540e3eccc68c659979d7858249bb
Nikita.Bumakov <Nikita.Bumakov@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-07-19 19:20:07 +0000

    - some refactoring

--- Android-Geocaching/src/su/geocaching/android/ui/info/GalleryImageAdapter.java
+++ Android-Geocaching/src/su/geocaching/android/ui/info/GalleryImageAdapter.java
@@ -2,6 +2,7 @@ package su.geocaching.android.ui.info;
 
 import java.io.File;
 import java.io.FilenameFilter;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -20,82 +21,82 @@ import su.geocaching.android.ui.R;
  */
 public class GalleryImageAdapter extends BaseAdapter {
 
-  private static final String TAG = GalleryImageAdapter.class.getCanonicalName();
+    private static final String TAG = GalleryImageAdapter.class.getCanonicalName();
 
-  private File[]  imageList;
-  private Context context;
+    private File[] imageList;
+    private Context context;
 
-  private final int thumbnailsPhotoSize = 125;
+    private final int thumbnailsPhotoSize = 125;
 
-  public GalleryImageAdapter(Context context) {
-    this.context = context;
-    LogManager.d(TAG, "new GalleryImageAdapter created");
-  }
-
-  public GalleryImageAdapter(final Context context, int cacheId) {
-    this(context);
-
-    File imagesDirectory = new File(Environment.getExternalStorageDirectory(), String.format(context.getString(R.string.cache_directory), cacheId));
-
-    FilenameFilter imageFilter = new FilenameFilter() {
-
-      @Override
-      public boolean accept(File dir, String name) {
-        return (name.endsWith(".jpg") || name.endsWith(".png"));
-      }
-    };
-    imageList = imagesDirectory.listFiles(imageFilter);
-  }
-
-  @Override
-  public int getCount() {
-    return imageList == null ? 0 : imageList.length;
-  }
-
-  @Override
-  public Object getItem(int i) {
-    return Uri.fromFile(imageList[i]);
-  }
-
-  @Override
-  public long getItemId(int i) {
-    return 0;
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    ImageView image;
-    if (convertView == null) { // if it's not recycled, initialize some attributes
-      image = new ImageView(context);
-      image.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT, GridView.LayoutParams.WRAP_CONTENT));
-      image.setScaleType(ImageView.ScaleType.CENTER_CROP);
-      image.setPadding(8, 8, 8, 8);
-
-    } else {
-      image = (ImageView) convertView;
+    public GalleryImageAdapter(Context context) {
+        this.context = context;
+        LogManager.d(TAG, "new GalleryImageAdapter created");
     }
 
-    Bitmap scaleBm = scaleBitmap(position);
-    if (scaleBm == null) {
-      image.setImageResource(R.drawable.no_photo);
-    } else {
-      image.setImageBitmap(scaleBm);
+    public GalleryImageAdapter(final Context context, int cacheId) {
+        this(context);
+
+        File imagesDirectory = new File(Environment.getExternalStorageDirectory(), String.format(context.getString(R.string.cache_directory), cacheId));
+
+        FilenameFilter imageFilter = new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return (name.endsWith(".jpg") || name.endsWith(".png"));
+            }
+        };
+        imageList = imagesDirectory.listFiles(imageFilter);
     }
 
-    return image;
-  }
-
-  private Bitmap scaleBitmap(int position) {
-    BitmapFactory.Options options = new BitmapFactory.Options();
-    options.inJustDecodeBounds = true;
-    String path = imageList[position].getAbsolutePath();
-    BitmapFactory.decodeFile(path, options);
-    if (options.outHeight == -1 || options.outWidth == -1) {
-      return null;
+    @Override
+    public int getCount() {
+        return imageList == null ? 0 : imageList.length;
     }
-    int scale = Math.max(options.outHeight, options.outWidth) / thumbnailsPhotoSize + 1;
-    BitmapFactory.Options options2 = new BitmapFactory.Options();
-    options2.inSampleSize = scale;
-    return BitmapFactory.decodeFile(path, options2);
-  }
+
+    @Override
+    public Object getItem(int i) {
+        return Uri.fromFile(imageList[i]);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView image;
+        if (convertView == null) { // if it's not recycled, initialize some attributes
+            image = new ImageView(context);
+            image.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.WRAP_CONTENT, GridView.LayoutParams.WRAP_CONTENT));
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            image.setPadding(8, 8, 8, 8);
+
+        } else {
+            image = (ImageView) convertView;
+        }
+
+        Bitmap scaleBm = scaleBitmap(position);
+        if (scaleBm == null) {
+            image.setImageResource(R.drawable.no_photo);
+        } else {
+            image.setImageBitmap(scaleBm);
+        }
+
+        return image;
+    }
+
+    private Bitmap scaleBitmap(int position) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        String path = imageList[position].getAbsolutePath();
+        BitmapFactory.decodeFile(path, options);
+        if (options.outHeight == -1 || options.outWidth == -1) {
+            return null;
+        }
+        int scale = Math.max(options.outHeight, options.outWidth) / thumbnailsPhotoSize + 1;
+        BitmapFactory.Options options2 = new BitmapFactory.Options();
+        options2.inSampleSize = scale;
+        return BitmapFactory.decodeFile(path, options2);
+    }
 }
