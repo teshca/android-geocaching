e2be30c2d6a9c9fdb12b9a254678af6f7b9d1208
nickolay.artamonov@gmail.com <nickolay.artamonov@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-08-09 20:30:28 +0000

    - small about activity refactoring

--- trunk/Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/ui/AboutActivity.java
@@ -10,25 +10,24 @@ import su.geocaching.android.controller.managers.LogManager;
 
 public class AboutActivity extends Activity {
     private static final String TAG = AboutActivity.class.getCanonicalName();
-    private static final String ABOUT_ACTIVITY_FOLDER = "/AboutActivity";
+    private static final String ABOUT_ACTIVITY_NAME = "/AboutActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LogManager.d(TAG, "on create");
         setContentView(R.layout.about_activity);
 
         try {
-            this.setTitle(String.format(getString(R.string.application_version), getPackageManager().getPackageInfo(getPackageName(), 0).versionName));
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            this.setTitle(getString(R.string.about_application_version, versionName));
         } catch (NameNotFoundException e) {
             LogManager.e(TAG, e.getMessage(), e);
         }
-        Controller.getInstance().getGoogleAnalyticsManager().trackPageView(ABOUT_ACTIVITY_FOLDER);
+
+        Controller.getInstance().getGoogleAnalyticsManager().trackPageView(ABOUT_ACTIVITY_NAME);
     }
 
-
     public void onExitClick(View v) {
-        super.finish();
-        LogManager.d(TAG, "exit");
+        this.finish();
     }
 }
