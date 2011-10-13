ec50dfd74d268d30a7b46530cc627eb501bbfac3
nickolay.artamonov@gmail.com <nickolay.artamonov@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-10-13 17:19:53 +0000

    - Url encoding is not actually needed for Analitycs

--- branches/1.2/Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
+++ branches/1.2/Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
@@ -5,16 +5,12 @@ import su.geocaching.android.ui.R;
 import android.content.Context;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
-import java.net.URLEncoder;
-
 public class GoogleAnalyticsManager {
     private static final boolean DEBUG = Controller.DEBUG; // this constant is really need, because compiler can remove code blocks which cannot be execute
     private static final int DISPATCH_INTERVAL = 120; // seconds
     private GoogleAnalyticsTracker tracker;
-    private int applicationVersionCode;
 
     public GoogleAnalyticsManager(Context context) {
-        applicationVersionCode = Controller.getInstance().getApplicationVersionCode();
         if (!DEBUG) {
             tracker = GoogleAnalyticsTracker.getInstance();
             tracker.start(context.getString(R.string.id_Google_Analytics), DISPATCH_INTERVAL, context);
@@ -23,39 +19,33 @@ public class GoogleAnalyticsManager {
 
     public void trackActivityLaunch(String activityName) {
         if (!DEBUG) {
-            activityName = URLEncoder.encode(activityName);
             tracker.trackPageView(activityName);
         }
     }
 
     public void trackExternalActivityLaunch(String activityName) {
         if (!DEBUG) {
-            activityName = URLEncoder.encode(activityName);
             tracker.trackPageView("/external" + activityName);
         }
     }
 
     public void trackException(String category, String action, Throwable ex) {
         if (!DEBUG) {
-            String encodedCategory = category != null ? URLEncoder.encode(category) : "";
-            String encodedAction = action != null ? URLEncoder.encode(action) : "";
             String message = ex.getMessage() != null ? ex.getMessage() : "";
             StringBuffer stackTrace = new StringBuffer(message);
-            final String NEW_LINE = System.getProperty("line.separator");
+            final String NEW_LINE = "--//--";
             for (StackTraceElement s : ex.getStackTrace()) {
                 stackTrace.append(String.format(" at %s.%s(%s:%d)", s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber()));
                 stackTrace.append(NEW_LINE);
             }
-            tracker.trackEvent(encodedCategory, encodedAction, stackTrace.toString(), applicationVersionCode);
+            tracker.trackEvent(category, action, stackTrace.toString(), 0);
             tracker.dispatch();
         }
     }
 
     public void trackError(String category, String action) {
         if (!DEBUG) {
-            String encodedCategory = category != null ? URLEncoder.encode(category) : "";
-            String encodedAction = action != null ? URLEncoder.encode(action) : "";
-            tracker.trackEvent(encodedCategory, encodedAction, null, applicationVersionCode);
+            tracker.trackEvent(category, action, null, 0);
             tracker.dispatch();
         }
     }
