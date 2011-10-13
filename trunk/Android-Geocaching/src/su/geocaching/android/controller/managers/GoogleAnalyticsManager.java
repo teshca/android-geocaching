8c175da5b93ccc5385d023dd8443b77df4beafa5
nickolay.artamonov@gmail.com <nickolay.artamonov@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2011-10-13 18:12:10 +0000

    - report debug version to another profile

--- trunk/Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
+++ trunk/Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
@@ -1,54 +1,43 @@
 package su.geocaching.android.controller.managers;
 
 import su.geocaching.android.controller.Controller;
-import su.geocaching.android.ui.R;
 import android.content.Context;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 public class GoogleAnalyticsManager {
-    private static final boolean DEBUG = Controller.DEBUG; // this constant is really need, because compiler can remove code blocks which cannot be execute
     private static final int DISPATCH_INTERVAL = 120; // seconds
     private GoogleAnalyticsTracker tracker;
     private String applicationVersionName;
 
     public GoogleAnalyticsManager(Context context) {
         applicationVersionName = Controller.getInstance().getApplicationVersionName();
-        if (!DEBUG) {
-            tracker = GoogleAnalyticsTracker.getInstance();
-            tracker.start(context.getString(R.string.id_Google_Analytics), DISPATCH_INTERVAL, context);
-        }
+        String analyticsProfileId = Controller.DEBUG ? "UA-20327116-6" : "UA-20327116-3";
+        tracker = GoogleAnalyticsTracker.getInstance();
+        tracker.start(analyticsProfileId, DISPATCH_INTERVAL, context);
     }
 
     public void trackActivityLaunch(String activityName) {
-        if (!DEBUG) {
-            tracker.trackPageView(activityName);
-        }
+        tracker.trackPageView(activityName);
     }
 
     public void trackExternalActivityLaunch(String activityName) {
-        if (!DEBUG) {
-            tracker.trackPageView("/external" + activityName);
-        }
+        tracker.trackPageView("/external" + activityName);
     }
 
     public void trackException(String category, String action, Throwable ex) {
-        if (!DEBUG) {
-            String message = ex.getMessage() != null ? ex.getMessage() : "";
-            StringBuilder stackTrace = new StringBuilder(message);
-            final String NEW_LINE = "--//--";
-            for (StackTraceElement s : ex.getStackTrace()) {
-                stackTrace.append(String.format(" at %s.%s(%s:%d)", s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber()));
-                stackTrace.append(NEW_LINE);
-            }
-            tracker.trackEvent(category + ":" + applicationVersionName, action, stackTrace.toString(), 0);
-            tracker.dispatch();
+        String message = ex.getMessage() != null ? ex.getMessage() : "";
+        StringBuilder stackTrace = new StringBuilder(message);
+        final String NEW_LINE = "--//--";
+        for (StackTraceElement s : ex.getStackTrace()) {
+            stackTrace.append(String.format(" at %s.%s(%s:%d)", s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber()));
+            stackTrace.append(NEW_LINE);
         }
+        tracker.trackEvent(category + ":" + applicationVersionName, action, stackTrace.toString(), 0);
+        tracker.dispatch();
     }
 
     public void trackError(String category, String action) {
-        if (!DEBUG) {
-            tracker.trackEvent(category + ":" + applicationVersionName, action, null, 0);
-            tracker.dispatch();
-        }
+        tracker.trackEvent(category + ":" + applicationVersionName, action, null, 0);
+        tracker.dispatch();
     }
 }
