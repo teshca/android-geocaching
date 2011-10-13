1dd688207c4e08c81c2c8bb268f3ca734560f543
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2011-10-13 17:26:01 +0000

    - still we want to report version code

--- Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
+++ Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
@@ -9,8 +9,10 @@ public class GoogleAnalyticsManager {
     private static final boolean DEBUG = Controller.DEBUG; // this constant is really need, because compiler can remove code blocks which cannot be execute
     private static final int DISPATCH_INTERVAL = 120; // seconds
     private GoogleAnalyticsTracker tracker;
+    private int applicationVersionCode;
 
     public GoogleAnalyticsManager(Context context) {
+        applicationVersionCode = Controller.getInstance().getApplicationVersionCode();
         if (!DEBUG) {
             tracker = GoogleAnalyticsTracker.getInstance();
             tracker.start(context.getString(R.string.id_Google_Analytics), DISPATCH_INTERVAL, context);
@@ -32,20 +34,20 @@ public class GoogleAnalyticsManager {
     public void trackException(String category, String action, Throwable ex) {
         if (!DEBUG) {
             String message = ex.getMessage() != null ? ex.getMessage() : "";
-            StringBuffer stackTrace = new StringBuffer(message);
+            StringBuilder stackTrace = new StringBuilder(message);
             final String NEW_LINE = "--//--";
             for (StackTraceElement s : ex.getStackTrace()) {
                 stackTrace.append(String.format(" at %s.%s(%s:%d)", s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber()));
                 stackTrace.append(NEW_LINE);
             }
-            tracker.trackEvent(category, action, stackTrace.toString(), 0);
+            tracker.trackEvent(category + ":" + applicationVersionCode, action, stackTrace.toString(), 0);
             tracker.dispatch();
         }
     }
 
     public void trackError(String category, String action) {
         if (!DEBUG) {
-            tracker.trackEvent(category, action, null, 0);
+            tracker.trackEvent(category + ":" + applicationVersionCode, action, null, 0);
             tracker.dispatch();
         }
     }
