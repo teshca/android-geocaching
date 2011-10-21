3cf585500b40a3d73a5861bfc033e6684c9c258b
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2011-10-21 16:59:49 +0000

    - updated version  of analitycs

--- Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
+++ Android-Geocaching/src/su/geocaching/android/controller/managers/GoogleAnalyticsManager.java
@@ -3,9 +3,9 @@ package su.geocaching.android.controller.managers;
 import su.geocaching.android.controller.Controller;
 import android.content.Context;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+import su.geocaching.android.ui.R;
 
 public class GoogleAnalyticsManager {
-    private static final int DISPATCH_INTERVAL = 120; // seconds
     private GoogleAnalyticsTracker tracker;
     private String applicationVersionName;
 
@@ -13,31 +13,42 @@ public class GoogleAnalyticsManager {
         applicationVersionName = Controller.getInstance().getApplicationVersionName();
         String analyticsProfileId = Controller.DEBUG ? "UA-20327116-6" : "UA-20327116-3";
         tracker = GoogleAnalyticsTracker.getInstance();
-        tracker.start(analyticsProfileId, DISPATCH_INTERVAL, context);
+        tracker.setProductVersion(context.getString(R.string.app_name), applicationVersionName);
+        tracker.start(analyticsProfileId, context);
     }
 
     public void trackActivityLaunch(String activityName) {
         tracker.trackPageView(activityName);
+        tracker.dispatch();
     }
 
     public void trackExternalActivityLaunch(String activityName) {
         tracker.trackPageView("/external" + activityName);
+        tracker.dispatch();
     }
 
-    public void trackException(String category, String action, Throwable ex) {
+    public void trackCaughtException(String tag, Throwable ex) {
+        trackException("Caught exception", tag, ex);
+    }
+
+    public void trackUncaughtException(String tag, Throwable ex) {
+        trackException("Uncaught exception", tag, ex);
+    }
+
+    public void trackError(String tag, String message) {
+        tracker.trackEvent("Error message: " + applicationVersionName, tag, message, 0);
+        tracker.dispatch();
+    }
+
+    private void trackException(String category, String tag, Throwable ex) {
         String message = ex.getMessage() != null ? ex.getMessage() : "";
         StringBuilder stackTrace = new StringBuilder(message);
-        final String NEW_LINE = "--//--";
+        final String NEW_LINE = " _____ ";
         for (StackTraceElement s : ex.getStackTrace()) {
             stackTrace.append(String.format(" at %s.%s(%s:%d)", s.getClassName(), s.getMethodName(), s.getFileName(), s.getLineNumber()));
             stackTrace.append(NEW_LINE);
         }
-        tracker.trackEvent(category + ":" + applicationVersionName, action, stackTrace.toString(), 0);
-        tracker.dispatch();
-    }
-
-    public void trackError(String category, String action) {
-        tracker.trackEvent(category + ":" + applicationVersionName, action, null, 0);
+        tracker.trackEvent(category + ": " + applicationVersionName, tag, stackTrace.toString(), 0);
         tracker.dispatch();
     }
 }
