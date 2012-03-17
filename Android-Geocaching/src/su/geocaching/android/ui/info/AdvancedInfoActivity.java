10e58b7bc0be671cb8592be0d00d2fe3b25900eb
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2012-03-17 10:33:50 +0000

    - Scrollable tabs added to AdvancedInfoActivity

--- Android-Geocaching/src/su/geocaching/android/ui/info/AdvancedInfoActivity.java
+++ Android-Geocaching/src/su/geocaching/android/ui/info/AdvancedInfoActivity.java
@@ -1,17 +1,170 @@
 package su.geocaching.android.ui.info;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
 /**
  * Activity to display cache's information, notebook and photos
  *
  * @author Nickolay Artamonov
  */
+
+import java.util.ArrayList;
+
+import su.geocaching.android.ui.R;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ActionBar.Tab;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
 public class AdvancedInfoActivity extends FragmentActivity {
+    ViewPager  mViewPager;
+    TabsAdapter mTabsAdapter;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(su.geocaching.android.ui.R.layout.advanced_info_activity);
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        ActionBar.Tab tab1 = getSupportActionBar().newTab().setText(R.string.info_tab_name_info);
+        ActionBar.Tab tab2 = getSupportActionBar().newTab().setText(R.string.info_tab_name_notebook);
+        ActionBar.Tab tab3 = getSupportActionBar().newTab().setText(R.string.info_tab_name_photo);
+
+        mViewPager = (ViewPager)findViewById(R.id.pager);
+        mTabsAdapter = new TabsAdapter(this, getSupportActionBar(), mViewPager);
+        mTabsAdapter.addTab(tab1, AdvancedInfoActivity.CountingFragment.class);
+
+        mTabsAdapter.addTab(tab2, AdvancedInfoActivity.CountingFragment.class);
+        mTabsAdapter.addTab(tab3, AdvancedInfoActivity.CountingFragment.class);
+
+        if (savedInstanceState != null) {
+            getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt("index"));
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("index", getSupportActionBar().getSelectedNavigationIndex());
+    }
+
+    /**
+     * This is a helper class that implements the management of tabs and all
+     * details of connecting a ViewPager with associated TabHost.  It relies on a
+     * trick.  Normally a tab host has a simple API for supplying a View or
+     * Intent that each tab will show.  This is not sufficient for switching
+     * between pages.  So instead we make the content part of the tab host
+     * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
+     * view to show as the tab content.  It listens to changes in tabs, and takes
+     * care of switch to the correct paged in the ViewPager whenever the selected
+     * tab changes.
+     */
+    public static class TabsAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener, ActionBar.TabListener {
+        private final Context mContext;
+        private final ActionBar mActionBar;
+        private final ViewPager mViewPager;
+        private final ArrayList<String> mTabs = new ArrayList<String>();
+
+        public TabsAdapter(FragmentActivity activity, ActionBar actionBar, ViewPager pager) {
+            super(activity.getSupportFragmentManager());
+            mContext = activity;
+            mActionBar = actionBar;
+            mViewPager = pager;
+            mViewPager.setAdapter(this);
+            mViewPager.setOnPageChangeListener(this);
+        }
+
+        public void addTab(ActionBar.Tab tab, Class<?> clss) {
+            mTabs.add(clss.getName());
+            mActionBar.addTab(tab.setTabListener(this));
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return mTabs.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return Fragment.instantiate(mContext, mTabs.get(position), null);
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            mActionBar.setSelectedNavigationItem(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+
+        @Override
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            mViewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        }    
+    }
+    
+    public static class CountingFragment extends Fragment {
+        int mNum;
+
+        /**
+         * Create a new instance of CountingFragment, providing "num"
+         * as an argument.
+         */
+        static CountingFragment newInstance(int num) {
+            CountingFragment f = new CountingFragment();
+
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
+
+            return f;
+        }
+
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
+
+        /**
+         * The Fragment's UI is just a simple text view showing its
+         * instance number.
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.hello_world, container, false);
+            View tv = v.findViewById(R.id.text);
+            ((TextView)tv).setText("Fragment #" + mNum);
+            tv.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.gallery_thumb));
+            return v;
+        }
+    }    
 }
