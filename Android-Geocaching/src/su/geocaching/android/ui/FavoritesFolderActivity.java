ec1e654e323481e2fbb7ff2ea00fd43550ff5ae7
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2012-09-05 20:11:47 +0000

    - action bar replaced in favorites activity

--- Android-Geocaching/src/su/geocaching/android/ui/FavoritesFolderActivity.java
+++ Android-Geocaching/src/su/geocaching/android/ui/FavoritesFolderActivity.java
@@ -2,21 +2,20 @@ package su.geocaching.android.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
 import su.geocaching.android.controller.Controller;
 import su.geocaching.android.controller.adapters.FavoritesArrayAdapter;
 import su.geocaching.android.controller.managers.DbManager;
@@ -29,7 +28,7 @@ import java.util.List;
 /**
  * Class for create ListActivity with favorites caches
  */
-public class FavoritesFolderActivity extends ListActivity {
+public class FavoritesFolderActivity extends SherlockListActivity {
 
     private static final String TAG = FavoritesFolderActivity.class.getCanonicalName();
     private static final String FAVORITES_FOLDER = "/FavoritesActivity";
@@ -39,8 +38,6 @@ public class FavoritesFolderActivity extends ListActivity {
     private DbManager dbManager;
     private FavoritesArrayAdapter favoriteGeoCachesAdapter;
     private TextView tvNoCache;
-    private ImageView actionSearch;
-    private ImageView actionSort;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,16 +46,15 @@ public class FavoritesFolderActivity extends ListActivity {
         setContentView(R.layout.favorites_folder_activity);
 
         tvNoCache = (TextView) findViewById(R.id.tvNoCacheFound);
-        actionSearch = (ImageView) findViewById(R.id.actionSearch);
-        actionSort = (ImageView) findViewById(R.id.actionSort);
 
         dbManager = Controller.getInstance().getDbManager();
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         favoriteGeoCachesAdapter = new FavoritesArrayAdapter(this);
         favoriteGeoCachesAdapter.setSortType(FavoritesArrayAdapter.GeoCacheSortType.values()[Controller.getInstance().getPreferencesManager().getFavoritesSortType()]);
         getListView().setTextFilterEnabled(true);
         setListAdapter(favoriteGeoCachesAdapter);
-       
+        getListView().getOverScrollMode();
         Controller.getInstance().getGoogleAnalyticsManager().trackActivityLaunch(FAVORITES_FOLDER);
     }
     
@@ -73,13 +69,9 @@ public class FavoritesFolderActivity extends ListActivity {
 
         if (favoriteGeoCachesAdapter.isEmpty()) {
             tvNoCache.setVisibility(View.VISIBLE);
-            actionSearch.setVisibility(View.GONE);
-            actionSort.setVisibility(View.GONE);
         } else {
             tvNoCache.setVisibility(View.GONE);
-            actionSearch.setVisibility(View.VISIBLE);
-            actionSort.setVisibility(View.VISIBLE);
-        }        
+        }
     }
 
     @Override
@@ -91,19 +83,24 @@ public class FavoritesFolderActivity extends ListActivity {
         // Refresh list data
         refreshListData();
         // Restore previous state (including selected item index and scroll position)
-        getListView().onRestoreInstanceState(state);            
+        getListView().onRestoreInstanceState(state);
+
+        invalidateOptionsMenu();
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean listIsNotEmpty = favoriteGeoCachesAdapter.getAllItemsCount() > 0;
         boolean listIsNotFiltered = favoriteGeoCachesAdapter.getAllItemsCount() == favoriteGeoCachesAdapter.getCount();
-        menu.findItem(R.id.delete_all_cache_in_database).setEnabled(listIsNotFiltered && favoriteGeoCachesAdapter.getCount() > 0);
+        menu.findItem(R.id.menu_delete_all_caches).setVisible(listIsNotEmpty);
+        menu.findItem(R.id.menu_search).setVisible(listIsNotEmpty);
+        menu.findItem(R.id.menu_sort).setVisible(listIsNotEmpty);
         return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+        MenuInflater inflater = getSupportMenuInflater();
         inflater.inflate(R.menu.favorites_menu, menu);
         return true;
     }
@@ -111,7 +108,16 @@ public class FavoritesFolderActivity extends ListActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.delete_all_cache_in_database:
+            case android.R.id.home:
+                onHome();
+                return true;
+            case R.id.menu_search:
+                onSearchRequested();
+                return true;
+            case R.id.menu_sort:
+                showDialog(SORT_TYPE_DIALOG_ID);
+                return true;
+            case R.id.menu_delete_all_caches:
                 showDialog(DELETE_CACHE_DIALOG_ID);
                 return true;
             default:
@@ -119,18 +125,14 @@ public class FavoritesFolderActivity extends ListActivity {
         }
     }
 
+    private void onHome() {
+        NavigationManager.startDashboardActivity(this);
+    }
+
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         NavigationManager.startInfoActivity(this, ((GeoCache) this.getListAdapter().getItem(position)));
-    }
-
-    public void onHomeClick(View v) {
-        NavigationManager.startDashboardActivity(this);
-    }
-
-    public void onSearchClick(View v) {
-        onSearchRequested();
     }
 
     @Override
@@ -149,10 +151,6 @@ public class FavoritesFolderActivity extends ListActivity {
             }
         }
         return false;
-    }
-
-    public void onSortClick(View v) {
-        showDialog(SORT_TYPE_DIALOG_ID);
     }
 
     @Override
@@ -182,15 +180,15 @@ public class FavoritesFolderActivity extends ListActivity {
             }
             case DELETE_CACHE_DIALOG_ID: {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(String.format(this.getString(R.string.ask_delete_all_cache_in_database), favoriteGeoCachesAdapter.getAllItemsCount()))
+                builder.setTitle(R.string.favorites_activity_confirm_delete_title);
+                builder.setMessage(String.format(this.getString(R.string.ask_delete_all_caches_in_database), favoriteGeoCachesAdapter.getAllItemsCount()))
                 .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dbManager.clearDB();
                         favoriteGeoCachesAdapter.clear();
                         favoriteGeoCachesAdapter.notifyDataSetChanged();
                         tvNoCache.setVisibility(View.VISIBLE);
-                        actionSearch.setVisibility(View.GONE);
-                        actionSort.setVisibility(View.GONE);
+                        invalidateOptionsMenu();
                         dialog.cancel();
                     }
                 })
@@ -205,7 +203,6 @@ public class FavoritesFolderActivity extends ListActivity {
                 return null;
             }
         }
-
     }
 
     private void sort(FavoritesArrayAdapter.GeoCacheSortType sortType) {
