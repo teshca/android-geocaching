d9fc799d50a9daad4da8d87711711164975951e5
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2012-09-08 11:16:46 +0000

    - more native way to use actionbar in cache notes activity

--- Android-Geocaching/src/su/geocaching/android/ui/info/CacheNotesActivity.java
+++ Android-Geocaching/src/su/geocaching/android/ui/info/CacheNotesActivity.java
@@ -1,15 +1,16 @@
 package su.geocaching.android.ui.info;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import su.geocaching.android.controller.Controller;
 import su.geocaching.android.controller.managers.LogManager;
 import su.geocaching.android.controller.managers.NavigationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import su.geocaching.android.model.GeoCache;
+import su.geocaching.android.ui.R;
 
 /**
  * Activity for show and write notes about geocache
@@ -20,24 +21,17 @@ public class CacheNotesActivity extends SherlockActivity {
 
     private static String TAG = CacheNotesActivity.class.getCanonicalName();
     private EditText cacheNotesText;
+    private GeoCache geoCache;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final GeoCache geoCache = getIntent().getParcelableExtra(GeoCache.class.getCanonicalName());
+        geoCache = getIntent().getParcelableExtra(GeoCache.class.getCanonicalName());
 
         setContentView(su.geocaching.android.ui.R.layout.cache_notes_activity);
-        cacheNotesText = (EditText) findViewById(su.geocaching.android.ui.R.id.cache_notes_text);
-        Button saveButton = (Button) findViewById(su.geocaching.android.ui.R.id.text_save_button);
 
-        cacheNotesText.setText(Controller.getInstance().getDbManager().getNoteById(geoCache.getId()));
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Controller.getInstance().getDbManager().updateNotes(geoCache.getId(), cacheNotesText.getText().toString());
-                finish();
-            }
-        });
+        cacheNotesText = (EditText) findViewById(su.geocaching.android.ui.R.id.cache_notes_text);
 
         getSupportActionBar().setTitle(geoCache.getName());
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -45,12 +39,22 @@ public class CacheNotesActivity extends SherlockActivity {
         LogManager.d(TAG, "CacheNotesActivity created");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.cache_notes_menu, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 NavigationManager.startDashboardActivity(this);
+                return true;
+            case R.id.menu_cache_notes_save:
+                Controller.getInstance().getDbManager().updateNotes(geoCache.getId(), cacheNotesText.getText().toString());
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
