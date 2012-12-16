791e5790a98dbe8cd0f1ba9311a9c728f5a3a2f7
nickolay.artamonov@gmail.com <nickolay.artamonov@gmail.com@0d56dec3-407c-e8f4-c62f-2f924401f5c5>
2012-12-16 09:01:13 +0000

    - new geocahing.su icons project

---
+++ trunk/files/GeocachingIcons/GeocachingIcons/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GeocachingIcons
+{
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Canvas.SaveImage();
+        }
+	}
+}
