973d5b50e45500d73effb3ab726ca02a1b448961
nickolay.artamonov <Nickolay.Artamonov@gmail.com>
2013-09-12 20:16:06 +0000

    - updated projects

--- Android-Geocaching.Test/gen/com/actionbarsherlock/sample/demos/tests/R.java
+++ Android-Geocaching.Test/gen/com/actionbarsherlock/sample/demos/tests/R.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.google.android.gms;
+package com.actionbarsherlock.sample.demos.tests;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
